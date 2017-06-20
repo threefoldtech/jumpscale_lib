a8263581242095244f130435333dc88dc4a5119f
Maxime Daniel <root@maxux.net>
2017-06-20 18:04:11 +0200

    Fix g8core import name

--- JumpScale9Lib/clients/g8core/G8CoreFactory.py
+++ JumpScale9Lib/clients/g8core/G8CoreFactory.py
@@ -6,7 +6,7 @@ class G8CoreFactory():
 
     def __init__(self):
         self.__jslocation__ = "j.clients.g8core"
-        self.__imports__ = zeroos
+        self.__imports__ = g8core
 
     def get(self, host, port=6379, password=''):
         return g8core.Client(host=host, port=port, password=password)
