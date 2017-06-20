ad388ce83e4cd3610f3e400f464b41c2769501d9
Maxime Daniel <root@maxux.net>
2017-06-20 18:00:20 +0200

    Use zeroos.core0 insted of g8core, keeping g8core to keep code compatibility

--- JumpScale9Lib/clients/g8core/G8CoreFactory.py
+++ JumpScale9Lib/clients/g8core/G8CoreFactory.py
@@ -1,12 +1,12 @@
 # from JumpScale9 import j
-import g8core
+from zeroos.core0 import client as g8core
 
 
 class G8CoreFactory():
 
     def __init__(self):
         self.__jslocation__ = "j.clients.g8core"
-        self.__imports__ = g8core
+        self.__imports__ = zeroos
 
     def get(self, host, port=6379, password=''):
         return g8core.Client(host=host, port=port, password=password)
