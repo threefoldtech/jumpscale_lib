51df8a26645ae2983db40340d724d3e4bbf89edf
someid <kristof@incubaid.com>
2017-08-31 17:50:09 +0400

    111

--- JumpScale9Lib/clients/zero_os/ZeroOSFactory.py
+++ JumpScale9Lib/clients/zero_os/ZeroOSFactory.py
@@ -1,8 +1,37 @@
 from js9 import j
-
+from zeroos.orchestrator.sal.Node import Node
 import ovh
 import requests
 import time
+
+
+class ZeroOS:
+    def __init__(self, node):
+        self.node = node
+        self.client = node.client
+
+    def allow_port_tcp(self, port, interface=None, subnet=None):
+        for item in self.client.nft.list():
+            if item.startswith("tcp") and "accept" in item and str(port) in item:
+                return True
+        self.client.nft.open_port(port)
+        return True
+
+    def prepare_disks(self,  name="fscache", wipedisks=False):
+        """
+        if exists will return, if not will create
+        when wipedisks used will reset the disks (CAREFUL)
+        """
+        print("[+] prepare disks")
+        if wipedisks:
+            self.node.wipedisks()
+
+        res = self.node.find_persistance(name=name)
+        if res is not None:
+            return res
+        else:
+            res = self.node.ensure_persistance(name=name)
+        return res
 
 
 class ZeroOSFactory:
@@ -40,7 +69,8 @@ class ZeroOSFactory:
         zt = zerotierClient
 
         print("[+] booting server " + OVHServerID + " to zero-os")
-        task = cl.zeroOSBoot(target=OVHServerID, zerotierNetworkID=zerotierNetworkID)
+        task = cl.zeroOSBoot(target=OVHServerID,
+                             zerotierNetworkID=zerotierNetworkID)
         print("[+] waiting " + OVHServerID + " for reboot")
         cl.waitServerReboot(OVHServerID, task['taskId'])
         ip_pub = cl.serverGetDetail(OVHServerID)["ip"]
@@ -48,7 +78,8 @@ class ZeroOSFactory:
 
         while True:
             try:
-                member = zt.getNetworkMemberFromIPPub(ip_pub, networkId=zerotierNetworkID, online=True)
+                member = zt.getNetworkMemberFromIPPub(
+                    ip_pub, networkId=zerotierNetworkID, online=True)
                 ipaddr_priv = member["ipaddr_priv"][0]
                 break
             except RuntimeError as e:
@@ -78,26 +109,33 @@ class ZeroOSFactory:
         ipxe_base: change this to the version you want, use master branch by default
         """
 
-        valid_plan_types = ("Type 0", "Type 1", "Type 2" ,"Type 2A", "Type 3", "Type S")
+        valid_plan_types = ("Type 0", "Type 1", "Type 2",
+                            "Type 2A", "Type 3", "Type S")
         if plan_type not in valid_plan_types:
-            j.exceptions.Input("bad plan type %s. Valid plan type are %s" % (plan_type, ','.join(valid_plan_types)))
+            j.exceptions.Input("bad plan type %s. Valid plan type are %s" % (
+                plan_type, ','.join(valid_plan_types)))
 
         ipxe_url = "%s/%s" % (ipxe_base, zerotierNetworkID)
 
         hostname = server_name
 
         # find project id
-        project_ids = [project.id for project in packetnetClient.list_projects() if project.name == project_name]
+        project_ids = [project.id for project in packetnetClient.list_projects(
+        ) if project.name == project_name]
         if not project_ids:
-            raise j.exceptions.NotFound('No projects found with name %s' % project_name)
+            raise j.exceptions.NotFound(
+                'No projects found with name %s' % project_name)
         project_id = project_ids[0]
 
         # find service type id
-        project_devices = {device.hostname: device for device in packetnetClient.list_devices(project_id)}
+        project_devices = {
+            device.hostname: device for device in packetnetClient.list_devices(project_id)}
         if hostname not in project_devices:
-            plan_ids = [plan.id for plan in packetnetClient.list_plans() if plan.name == plan_type]
+            plan_ids = [plan.id for plan in packetnetClient.list_plans()
+                        if plan.name == plan_type]
             if not plan_ids:
-                raise j.exceptions.NotFound('No plans found with name %s' % plan_type)
+                raise j.exceptions.NotFound(
+                    'No plans found with name %s' % plan_type)
             plan_id = plan_ids[0]
 
             # find facility id
@@ -107,13 +145,15 @@ class ZeroOSFactory:
                     facility_id = facility.id
 
             if facility_id is None:
-                raise j.exceptions.Input(message="Could not find facility in packet.net:%s" % location)
+                raise j.exceptions.Input(
+                    message="Could not find facility in packet.net:%s" % location)
             try:
                 device = packetnetClient.create_device(project_id=project_id, hostname=hostname, plan=plan_id,
-                                            facility=facility_id, operating_system='custom_ipxe', ipxe_script_url=ipxe_url)
+                                                       facility=facility_id, operating_system='custom_ipxe', ipxe_script_url=ipxe_url)
             except Exception as e:
                 if "Service Unavailable" in str(e):
-                    raise j.exceptions.Input(message="could not create packet.net machine, type of machine not available")
+                    raise j.exceptions.Input(
+                        message="could not create packet.net machine, type of machine not available")
                 raise e
         else:
             device = project_devices[hostname]
@@ -129,11 +169,13 @@ class ZeroOSFactory:
         if device.state != 'active':
             raise RuntimeError('Too long to provision')
 
-        ip_pub = [netinfo['address'] for netinfo in device.ip_addresses if netinfo['public'] and netinfo['address_family'] == 4]
+        ip_pub = [netinfo['address']
+                  for netinfo in device.ip_addresses if netinfo['public'] and netinfo['address_family'] == 4]
 
         while True:
             try:
-                member = zerotierClient.getNetworkMemberFromIPPub(ip_pub[0], networkId=zerotierNetworkID, online=True)
+                member = zerotierClient.getNetworkMemberFromIPPub(
+                    ip_pub[0], networkId=zerotierNetworkID, online=True)
                 ipaddr_priv = member["ipaddr_priv"][0]
                 break
             except RuntimeError as e:
@@ -150,3 +192,19 @@ class ZeroOSFactory:
         print("[+] zerotier IP: %s" % ipaddr_priv)
 
         return ip_pub, ipaddr_priv
+
+    def get(self, ip_pub, ip_priv):
+        print("[+] contacting zero-os server: %s" % ip_priv)
+        while True:
+            try:
+                node = Node(ip_priv)
+                node.client.timeout = 180
+                break
+
+            except RuntimeError as e:
+                print(
+                    "[+] make sure the server with public ip address %s is authorized in the zertotier network" % ip_pub)
+                time.sleep(1)
+                pass
+
+        return ZeroOS(node)
--- JumpScale9Lib/clients/zerotier/ZerotierFactory.py
+++ JumpScale9Lib/clients/zerotier/ZerotierFactory.py
@@ -5,26 +5,82 @@ import requests
 import json
 
 
+class ZerotierClientInteral:
+    def __init__(self, apikey):
+        self.apikey = apikey
+        self.apibase = "https://my.zerotier.com/api"
+
+    def request(self, path, data=None):
+        if data == None:
+            return requests.get(self.apibase + path, headers={'Authorization': 'Bearer ' + self.apikey})
+
+        else:
+            return requests.post(self.apibase + path, headers={'Authorization': 'Bearer ' + self.apikey}, json=data)
+
+    def delete(self, path):
+        return requests.delete(self.apibase + path, headers={'Authorization': 'Bearer ' + self.apikey})
+
+    def status(self):
+        return self.request("/status").json()
+
+    def user(self, id):
+        return self.request("/user/%s" % id).json()
+
+    def networks(self):
+        return self.request("/network").json()
+
+    # def network_create(self, name):
+    #     data = {
+    #         "config": {
+    #             "name": name,
+    #             "rules": [{"ruleNo": 1, "action": "accept"}],
+    #             "v4AssignMode": "zt",
+    #             "routes": [{"target": "10.147.17.0/24", "via": None, "flags": 0, "metric": 0}],
+    #             "ipAssignmentPools": [{"ipRangeStart": "10.147.17.1", "ipRangeEnd": "10.147.17.254"}]
+    #         }
+    #     }
+
+    #     return self.request("/network", data).json()
+
+    def network_delete(self, id):
+        return self.delete("/network/%s" % id)
+
+
 class ZerotierClient:
 
     def __init__(self, token):
         self.client = zerotier.client.Client()
         self.client.set_auth_header("Bearer " + token)
+        self._client = ZerotierClientInteral(token)
 
-    def getNetworks(self):
+    def memberAuthorize(self, zerotierNetworkId, name, macaddr, description=""):
+        members = self.networkMembersGet(zerotierNetworkId, online=True)
+        for member in members:
+            if member["authorized"] is False:
+                id = member["id"]
+                topost = {}
+                topost["name"] = name
+                topost["description"] = description
+                topost["authorized"] = True
+                print(886)
+                from IPython import embed
+                embed(colors='Linux')
+                self._client.request("/api/network/%s/member/%s" %
+                                     (zerotierNetworkId, id), topost)
+                self._client.request("/api/network/%s" % (zerotierNetworkId))
+
+    def networksGet(self):
         """
         returns [(id,name,onlinecount)]
         """
         res0 = self.client.network.listNetworks().json()
         res = []
         for item in res0:
-            if "name" in item:
-                res.append((item["id"], item["name"],
-                            item["onlineMemberCount"]))
-
+            res.append((item["id"], item["config"]["name"],
+                        item["onlineMemberCount"]))
         return res
 
-    def getNetworkMembers(self, networkId, online=True):
+    def networkMembersGet(self, networkId, online=True):
 
         res = self.client.network.listMembers(id=networkId).json()
         result = []
@@ -46,7 +102,7 @@ class ZerotierClient:
             result.append(res2)
         return result
 
-    def getNetworkMemberFromIPPub(self, ip_pub, networkId, online=True):
+    def networkMemberGetFromIPPub(self, ip_pub, networkId, online=True):
         res = self.getNetworkMembers(networkId, online)
 
         res = [item for item in res if item['ipaddr_pub'] == ip_pub]
@@ -56,46 +112,6 @@ class ZerotierClient:
                 "Did not find network member with ipaddr:%s" % ip_pub)
 
         return res[0]
-
-        # class ZerotierClient:
-        #     def __init__(self, apikey):
-        #         self.apikey = apikey
-        #         self.apibase = "https://my.zerotier.com/api"
-        #
-        #     def request(self, path, data=None):
-        #         if data == None:
-        #             return requests.get(self.apibase + path, headers={'Authorization': 'Bearer ' + self.apikey})
-        #
-        #         else:
-        #             return requests.post(self.apibase + path, headers={'Authorization': 'Bearer ' + self.apikey}, json=data)
-        #
-        #     def delete(self, path):
-        #         return requests.delete(self.apibase + path, headers={'Authorization': 'Bearer ' + self.apikey})
-        #
-        #     def status(self):
-        #         return self.request("/status").json()
-        #
-        #     def user(self, id):
-        #         return self.request("/user/%s" % id).json()
-        #
-        #     def networks(self):
-        #         return self.request("/network").json()
-        #
-        #     def network_create(self, name):
-        #         data = {
-        #             "config": {
-        #                 "name": name,
-        #                 "rules": [{"ruleNo": 1, "action": "accept"}],
-        #                 "v4AssignMode": "zt",
-        #                 "routes": [{"target": "10.147.17.0/24", "via": None, "flags": 0, "metric": 0}],
-        #                 "ipAssignmentPools": [{"ipRangeStart": "10.147.17.1", "ipRangeEnd": "10.147.17.254"}]
-        #             }
-        #         }
-        #
-        #         return self.request("/network", data).json()
-        #
-        #     def network_delete(self, id):
-        #         return self.delete("/network/%s" % id)
 
 
 class ZerotierFactory:
