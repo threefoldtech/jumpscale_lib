ecab26a462019f45dfaeb70f2c14301254c8547a
despiegk <kristof@incubaid.com>
2017-09-05 07:55:53 +0200

    docgenerator

--- JumpScale9Lib/tools/docgenerator/DocGenerator.py
+++ JumpScale9Lib/tools/docgenerator/DocGenerator.py
@@ -83,6 +83,7 @@ class DocGenerator:
         prefab = j.tools.prefab.local
         if prefab.core.doneGet("docgenerator:installed") == False or reset:
             prefab.development.nodejs.install()
+            prefab.development.nodejs.phantomjs()
             if "darwin" in str(j.core.platformtype.myplatform):
                 prefab.core.run("brew install graphviz")
                 prefab.core.run("brew install hugo")
@@ -90,14 +91,13 @@ class DocGenerator:
             elif "ubuntu" in str(j.core.platformtype.myplatform):
                 prefab.package.install('graphviz')
                 # Using package install will result in an old version on some machines
-                # prefab.core.file_download('https://github.com/spf13/hugo/releases/download/v0.20.7/hugo_0.20.7_Linux-64bit.tar.gz')
-                # prefab.core.file_expand('$TMPDIR/hugo_0.20.7_Linux-64bit.tar.gz')
-                # prefab.core.file_copy('$TMPDIR/hugo_0.20.7_Linux-64bit/hugo', '/usr/local/bin')
+                prefab.core.file_download('https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz')
+                prefab.core.file_expand('$TMPDIR/hugo_0.26_Linux-64bit.tar.gz')
+                prefab.core.file_copy('$TMPDIR/hugo_0.26_Linux-64bit/hugo', '/usr/bin/')
             prefab.development.golang.install()
             prefab.apps.caddy.build()
-            prefab.core.run("npm install -g phantomjs-prebuilt", profile=True)
             prefab.core.run("npm install -g mermaid", profile=True)            
-            prefab.apps.caddy.start()
+            prefab.apps.caddy.configure()
             prefab.core.doneSet("docgenerator:installed")
 
     def startWebserver(self):
@@ -130,7 +130,7 @@ class DocGenerator:
 
     def init(self):
         if not self._initOK:
-            self.installDeps()
+            self.install()
             j.sal.fs.remove(self._macroCodepath)
             # load the default macro's
             self.loadMacros("https://github.com/Jumpscale/docgenerator/tree/master/macros")
