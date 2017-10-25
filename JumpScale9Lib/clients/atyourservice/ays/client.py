1b60b0bf45f4f269aebe7ed29ecdd689c9dc04de
yves <yves@gig.tech>
2017-10-25 14:43:27 +0200

    fix addTemplates and listTemplates

--- JumpScale9Lib/clients/atyourservice/ays/client.py
+++ JumpScale9Lib/clients/atyourservice/ays/client.py
@@ -17,7 +17,7 @@ class Client:
             jwt = self._getJWT(clientID, secret, validity)
             self._set_auth_header('Bearer {}'.format(jwt))
 
-    def addTemplates(repo_url, branch):
+    def addTemplates(self, repo_url, branch):
         """
         Adds AYS template from a give Git repository
         Args:
@@ -27,7 +27,7 @@ class Client:
         data = {'url': '{repo_url}','branch': '{branch}'}
         self._ayscl.addTemplateRepo(data)
 
-    def listTemplates():
+    def listTemplates(self):
         """
         Returns a list of all (global) AYS templates.
         """
