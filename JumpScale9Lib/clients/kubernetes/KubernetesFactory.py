446f188391ea1da7bff35308ae41dc819d5c8fdb
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2017-12-24 16:30:57 +0200

    9.3.0 kubernetes (#91)
    * WIP: adding kubernetes client for phusion image
    
    * addded pod class to kubernetes client
    
    * added some doc strings
    
    * returned a prefab module with connection to deployed container
    
    * added read me to kub client , fixed some bugs in import
    
    * added some docs and missing parameters
    
    * adjusted the define container to include env vars
    
    * new adjustmentes for better object consistency
    
    the gets and lists now return objects that have delete update and create
    as functions instead of just data
    
    * adjustments for new way to get ip
    
    * added cluster definitions to client
    
    * added volumes to kubernetes clients
    
    * added incluster connection suppport

--- JumpScale9Lib/clients/kubernetes/KubernetesFactory.py
+++ JumpScale9Lib/clients/kubernetes/KubernetesFactory.py
@@ -1,35 +1,58 @@
 from js9 import j
-
-# pip install kube
+from kubernetes import client, config
 
 
 class KubernetesFactory:
+    """
+    kubernetes client factory each instance can relate to either a config file or a context or both
+    """
 
     def __init__(self):
         self.__jslocation__ = "j.clients.kubernetes"
 
-    def get(self, name, ...):
-        return KubernetesCluster()
-
-
-class KubernetesCluster():
-
-    def __init__(self, ...):
-        pass
-
-    def ubuntu1604_install(self, sshkey=None):
+    def get(self, config_path=None, context=None, ssh_key_path=None, incluster_config=False):
         """
-        deploy base ubuntu container on kubernetes host
+        Get an instance of the kubernetes class loaded with config_path.
+        If no path is given the default '${HOME}/.kube/config' path will be loaded.
+        IMPORTANT Please follow instructions at
+        https://developers.google.com/identity/protocols/application-default-credentials,
+        to setup credentials.
 
-        use specified sshkey is name of key !
-
-        if empty use the configured in jumpscale
-
-        @return prefab connection !
-
+        @param config_path,, str: full path to configuration file.
+        @param context ,, str: context name usually the same as the cluster used for isolation on same nodes
         """
-        if keyname is None:
-            keyname = j.core.state.configMe["ssh"]["sshkeyname"]
+        from .Kubernetes import KubernetesMaster as K8s
+        return K8s(config_path, context=context, ssh_key_path=ssh_key_path, incluster_config=incluster_config)
 
-    def deploy():
-        raise NotImplemented("Not implemented")
+
+    def create_config(self, config, path=None):
+        """
+        create config file.
+
+        @param config ,, dict the configurations in dict format
+        @param path ,, str full path to location the file should be saved will default to HOMEDIR/.kube/config
+        """
+        if not path:
+            directory = '%s/.kube/' % j.dirs.HOMEDIR
+            j.sal.fs.createDir(directory)
+            path = j.sal.fs.joinbPaths(directory, 'config')
+        data = j.data.serializer.yaml.dumps(config)
+        j.sal.fs.writeFile(path, data)
+        j.logger.logging.info('file saved at %s' % path)
+
+    def test(self):
+        """
+        TODO WIP
+        """
+
+        kub = self.get()
+        kub.list_clusters()
+        kub.list_deployments()
+        kub.list_nodes()
+        kub.list_pods()
+        kub.list_services()
+        prefab = kub.deploy_ubuntu1604('tester')
+        prefab.core.run('ls')
+
+
+
