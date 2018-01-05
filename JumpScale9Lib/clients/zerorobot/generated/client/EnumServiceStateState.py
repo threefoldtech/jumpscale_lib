5e736ac862eff6293e53b2a924ee31c55d246dc5
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-01-05 13:53:07 +0100

    enable 0-robot client with configmanager

---
+++ JumpScale9Lib/clients/zerorobot/generated/client/EnumServiceStateState.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class EnumServiceStateState(Enum):
+    ok = "ok"
+    error = "error"
---
+++ JumpScale9Lib/clients/zerorobot/generated/client/services_service.py
@@ -0,0 +1,177 @@
+
+from .Action import Action
+from .Error import Error
+from .Service import Service
+from .Task import Task
+from .api_response import APIResponse
+from .unhandled_api_error import UnhandledAPIError
+from .unmarshall_error import UnmarshallError
+
+
+class ServicesService:
+    def __init__(self, client):
+        self.client = client
+
+    def ListActions(self, service_guid, headers=None, query_params=None, content_type="application/json"):
+        """
+        List all the possible action a service can do.
+        It is method for GET /services/{service_guid}/actions
+        """
+        uri = self.client.base_url + "/services/" + service_guid + "/actions"
+        resp = self.client.get(uri, None, headers, query_params, content_type)
+        try:
+            if resp.status_code == 200:
+                resps = []
+                for elem in resp.json():
+                    resps.append(Action(elem))
+                return APIResponse(data=resps, response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
+
+    def GetTask(self, task_guid, service_guid, headers=None, query_params=None, content_type="application/json"):
+        """
+        Retrieve the detail of a task
+        It is method for GET /services/{service_guid}/task_list/{task_guid}
+        """
+        uri = self.client.base_url + "/services/" + service_guid + "/task_list/" + task_guid
+        resp = self.client.get(uri, None, headers, query_params, content_type)
+        try:
+            if resp.status_code == 200:
+                return APIResponse(data=Task(resp.json()), response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
+
+    def getTaskList(self, service_guid, headers=None, query_params=None, content_type="application/json"):
+        """
+        Return all the action in the task list
+        It is method for GET /services/{service_guid}/task_list
+        """
+        uri = self.client.base_url + "/services/" + service_guid + "/task_list"
+        resp = self.client.get(uri, None, headers, query_params, content_type)
+        try:
+            if resp.status_code == 200:
+                resps = []
+                for elem in resp.json():
+                    resps.append(Task(elem))
+                return APIResponse(data=resps, response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
+
+    def AddTaskToList(self, data, service_guid, headers=None, query_params=None, content_type="application/json"):
+        """
+        Add a task to the task list
+        It is method for POST /services/{service_guid}/task_list
+        """
+        uri = self.client.base_url + "/services/" + service_guid + "/task_list"
+        resp = self.client.post(uri, data, headers, query_params, content_type)
+        try:
+            if resp.status_code == 201:
+                return APIResponse(data=Task(resp.json()), response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
+
+    def DeleteService(self, service_guid, headers=None, query_params=None, content_type="application/json"):
+        """
+        Delete a service
+        It is method for DELETE /services/{service_guid}
+        """
+        uri = self.client.base_url + "/services/" + service_guid
+        return self.client.delete(uri, None, headers, query_params, content_type)
+
+    def GetService(self, service_guid, headers=None, query_params=None, content_type="application/json"):
+        """
+        Get the detail of a service
+        It is method for GET /services/{service_guid}
+        """
+        uri = self.client.base_url + "/services/" + service_guid
+        resp = self.client.get(uri, None, headers, query_params, content_type)
+        try:
+            if resp.status_code == 200:
+                return APIResponse(data=Service(resp.json()), response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
+
+    def listServices(self, headers=None, query_params=None, content_type="application/json"):
+        """
+        List all the services known by the ZeroRobot.
+        It is method for GET /services
+        """
+        uri = self.client.base_url + "/services"
+        resp = self.client.get(uri, None, headers, query_params, content_type)
+        try:
+            if resp.status_code == 200:
+                resps = []
+                for elem in resp.json():
+                    resps.append(Service(elem))
+                return APIResponse(data=resps, response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
+
+    def createService(self, data, headers=None, query_params=None, content_type="application/json"):
+        """
+        create a new service
+        It is method for POST /services
+        """
+        uri = self.client.base_url + "/services"
+        resp = self.client.post(uri, data, headers, query_params, content_type)
+        try:
+            if resp.status_code == 201:
+                return APIResponse(data=Service(resp.json()), response=resp)
+
+            message = 'unknown status code={}'.format(resp.status_code)
+            raise UnhandledAPIError(response=resp, code=resp.status_code,
+                                    message=message)
+        except ValueError as msg:
+            raise UnmarshallError(resp, msg)
+        except UnhandledAPIError as uae:
+            raise uae
+        except Exception as e:
+            raise UnmarshallError(resp, e.message)
