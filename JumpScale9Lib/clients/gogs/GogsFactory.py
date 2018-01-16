2e653c6819855bc5a8a4c597576e6d423b36c7b7
andrewayoub <andrew.a.mikhaeel@gmail.com>
2018-01-16 12:24:39 +0200

    remove get from gogs client factory

--- JumpScale9Lib/clients/gogs/GogsFactory.py
+++ JumpScale9Lib/clients/gogs/GogsFactory.py
@@ -10,23 +10,3 @@ class GogsFactory(JSConfigBaseFactory):
         self.__imports__ = "requests,psycopg2"
         self.logger = j.logger.get("j.clients.gogs")
         JSConfigBaseFactory.__init__(self, GogsClient)
-
-    def getRestClient(self, addr='https://127.0.0.1', port=3000, login='root', passwd='root', accesstoken=None):
-        """
-        # Getting client via accesstoken
-
-        # Create access token in gogs
-
-        Under user profile click your settings.
-        Click Applications, from there use generate new token to create your token.
-
-        # User Access token
-        rest = j.clients.gogs.getRestClient('https://docs.greenitglobe.com', 443,
-                                            accesstoken='myaccesstoken')
-
-        # Getting client via username, password
-        rest = j.clients.gogs.getRestClient('https://docs.greenitglobe.com', 443,
-                                            'myusername', 'mypassword')
-
-        """
-        return GogsClient(addr=addr, port=port, login=login, passwd=passwd, accesstoken=accesstoken)
