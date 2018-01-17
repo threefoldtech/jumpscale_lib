73b9ae82bf6cb1d89d59792a71f2cc3d3ca5b0e1
andrewayoub <andrew.a.mikhaeel@gmail.com>
2018-01-17 16:02:00 +0200

    refactor kubernetes client and factory to use js config

--- JumpScale9Lib/clients/kubernetes/KubernetesFactory.py
+++ JumpScale9Lib/clients/kubernetes/KubernetesFactory.py
@@ -1,28 +1,16 @@
 from js9 import j
 from kubernetes import client, config
+from .Kubernetes import KubernetesMaster
 
-
-class KubernetesFactory:
+JSConfigBaseFactory = j.tools.configmanager.base_class_configs
+class KubernetesFactory(JSConfigBaseFactory):
     """
     kubernetes client factory each instance can relate to either a config file or a context or both
     """
 
     def __init__(self):
         self.__jslocation__ = "j.clients.kubernetes"
-
-    def get(self, config_path=None, context=None, ssh_key_path=None, incluster_config=False):
-        """
-        Get an instance of the kubernetes class loaded with config_path.
-        If no path is given the default '${HOME}/.kube/config' path will be loaded.
-        IMPORTANT Please follow instructions at
-        https://developers.google.com/identity/protocols/application-default-credentials,
-        to setup credentials.
-
-        @param config_path,, str: full path to configuration file.
-        @param context ,, str: context name usually the same as the cluster used for isolation on same nodes
-        """
-        from .Kubernetes import KubernetesMaster as K8s
-        return K8s(config_path, context=context, ssh_key_path=ssh_key_path, incluster_config=incluster_config)
+        JSConfigBaseFactory.__init__(self, KubernetesMaster)
 
     def create_config(self, config, path=None):
         """
