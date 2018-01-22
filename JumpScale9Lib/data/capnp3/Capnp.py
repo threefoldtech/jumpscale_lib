threefoldtech/jumpscale_lib

commit f2e708a4075a046d72d03b9239908089cbb0a166

bchain_db

JumpScale9Lib/data/capnp3/ModelBaseData3.py
@@ -1,0 +1,38 @@
+from js9 import j
+# from collections import OrderedDict
+
+from .ModelBase import ModelBase
+
+class ModelBaseData(ModelBase):
+
+    def __init__(self, key="", new=False, collection=None):
+        super().__init__(key=key, new=new, collection=collection)
+        self._data_schema = None
+        self._data = None
+
+    @property
+    def data(self):
+        if self._data is None:
+            self._data = j.data.capnp.getObj(self.dbobj.dataSchema, binaryData=self.dbobj.data)
+        return self._data
+
+    @property
+    def dataSchema(self):
+        return j.data.capnp.getSchema(self.dbobj.dataSchema)
+
+    @property
+    def dataJSON(self):
+        return j.data.capnp.getJSON(self.data)
+
+    @property
+    def dataBinary(self):
+        return j.data.capnp.getBinaryData(self.data)
+
+
+def getText(text):
+    return str(object=text)
+
+
+def getInt(nr):
+    return int(nr)
+

JumpScale9Lib/clients/ovh/OVHClient.py
@@ -22,6 +22,9 @@ JSConfigBase = j.tools.configmanager.base_class_config
 
 
 class OVHClient(JSConfigBase):
+    """
+    
+    """
 
     def __init__(self, instance, data={}, parent=None):
         JSConfigBase.__init__(self, instance=instance,
@@ -137,18 +140,6 @@ class OVHClient(JSConfigBase):
     #             raise e
     #     return res
 
-    # def prefabsGet(self):
-    #     """
-    #     return all prefab connections to all known servers
-
-    #     returns [(name,prefab),]
-    #     """
-    #     res = []
-    #     for name in self.serversList:
-    #         details = self.server_detail_get(name)
-    #         e = j.tools.executor.get(details["ip"])
-    #         res.append((name, e.prefab))
-    #     return res
 
     def servers_install_wait(self):
         nrInstalling = 1
@@ -167,7 +158,7 @@ class OVHClient(JSConfigBase):
         self.details = {}
         print("INSTALL DONE")
 
-    def server_install(self, name, ovh_id="", installationTemplate="ubuntu1704-server_64", sshKeyName=None,
+    def server_install(self, name, ovh_id="", installationTemplate="ubuntu1710-server_64", sshKeyName=None,
                        useDistribKernel=True, noRaid=True, hostname="", wait=True):
         """
 
@@ -186,12 +177,11 @@ class OVHClient(JSConfigBase):
             if len(items) != 1:
                 raise RuntimeError(
                     "sshkeyname needs to be specified or only 1 sshkey needs to be loaded")
-            raise RuntimeError("not implemented yet")
-            # TODO:*1 need to check if the found key is already in OVH, if not upload to OVH
+            sshKeyName = items[0]
 
         if sshKeyName not in self.sshkeys_get():
-            raise j.exceptions.Input(message="could not find sshKeyName:%s" %
-                                     sshKeyName, level=1, source="", tags="", msgpub="")
+            pubkey=j.clients.ssh.SSHKeyGetFromAgentPub(sshKeyName)
+            self.sshkey_add(name, pubkey)            
 
         if name == "":
             raise j.exceptions.Input(

JumpScale9Lib/clients/packetnet/PacketNetFactory.py
@@ -222,11 +222,20 @@ class PacketNet(JSConfigClient):
 
             ssh.execute("ls /")
 
-            return device, ssh.prefab
+        conf = {}
+        conf ["facility"] = facility
+        conf ["netinfo"] = res["ip_addresses"]
+        conf ["plan"] = plan
+        conf ["hostname"] = hostname
+        conf ["project_id"] = self.projectid
+        conf ["os"] = os
+        conf ["ipxeUrl"] = ipxeUrl
 
-        j.tools.develop.nodes.nodeSet(name=hostname, addr=ipaddr, port=22, cat='packet', description='', selected=True)
+        node = j.tools.develop.nodes.set(name=hostname, addr=ipaddr, port=22, cat='packet', description='', selected=True,clienttype="j.clients.packetnet")
+        node.client = self
+        node.pubconfig = conf
 
-        return ipaddr
+        return node
 
     def addSSHKey(self, sshkeyPub):
         raise RuntimeError("not implemented")

JumpScale9Lib/data/capnp3/BCDB.py
@@ -1,0 +1,66 @@
+
+from js9 import j
+from pprint import pprint as print
+
+from .BCDB_Namespace import *
+
+class BCDB():
+
+    def __init__(self):
+        self.__jslocation__ = "j.data.bcdb"        
+        
+
+    def get(self,namespace):
+        return BCDB_Namespace(namespace)
+
+    def test(self):
+        """
+        js9 'j.data.bcdb.test()'
+
+        """
+
+        db=self.get("myns")
+
+        import nltk
+        # nltk.download('brown')
+        from nltk.corpus import brown
+
+        
+        nr=1000
+        namespace="myns"        
+        words= brown.words()[:nr*10]
+        def getword(words):
+            found=""
+            while len(found)<5:
+                picknr=j.data.idgenerator.generateRandomInt(0,len(words1))
+                found=words[picknr]
+            return found
+
+
+        pos=0
+        name1=""
+        words1=[]
+
+        j.tools.timer.start("perftest_index")
+        for i in range(nr):
+            rand=j.data.idgenerator.generateRandomInt(1,8)#to create sentences
+            toindex=""
+            for i in range(rand):
+                picknr=j.data.idgenerator.generateRandomInt(0,nr*10-1)
+                word=words[picknr]
+                words1.append(word)
+                toindex+=" %s"%word
+
+            toindex=toindex.strip()
+            db.index_add_term(toindex,i)
+
+        j.tools.timer.stop(nr)
+
+        w1=getword(words1)
+        w2=getword(words1)
+        w3=getword(words1)
+        res=db.find([w1])
+        res1=db.find([w2,w3])
+
+
+        from IPython import embed;embed(colors='Linux')

JumpScale9Lib/data/capnp3/BCDB_Namespace.py
@@ -1,0 +1,131 @@
+
+from js9 import j
+from pprint import pprint as print
+from functools import reduce
+
+class BCDB_Namespace():
+
+    def __init__(self,namespace):
+        self.db=j.clients.redis.get4core()
+        self.namespace = namespace
+
+        self.path = "%s/bcdc/%s/"%(j.dirs.VARDIR,self.namespace )
+        j.sal.fs.createDir(self.path)
+
+    
+    
+    def _hsetKeys(self,name):
+        if len(name)>3:
+            pre=self.namespace+"_"+name[0:2]
+            post=name[2:]
+        else:
+            pre=self.namespace
+            post=name
+        return pre,post
+
+    def index_add_term(self,name,uid):
+        """
+        builds a dense index in redis
+        will first make the text dense & remove all unrelevant items
+        then will take all words out
+        and store the words with references to the uid in binary form
+
+        """
+        res = j.data.nltk.dense(name,removespaces=False,word_minsize=4)
+        if res=="":
+            return
+        print(res)
+
+        for item in res.split(" "):
+
+            pre,post=self._hsetKeys(item)
+            #pre & post for performance & mem usage
+
+            bdata=self.db.hget(pre,post)
+            if bdata==None:
+                #does not exist yet, need to add
+                bdata=j.tools.numtools.listint_to_bin([uid])
+                self.db.hset(pre,post,bdata)
+                print("hset:%s:%s:%s"%(pre,post,[uid]))
+            else:
+                print("exist")
+                llist=j.tools.numtools.bin_to_listint(bdata)
+                if uid not in llist:
+                    llist.append(uid)
+                    bdata=j.tools.numtools.listint_to_bin(llist)
+                    self.db.hset(pre,post,bdata)
+                    print("hset:%s:%s:%s"%(pre,post,llist))
+
+    def find(self,terms):
+        res=[]
+        for term in terms:
+            term = j.data.nltk.dense(term,removespaces=False,word_minsize=4)
+            if " " in term:
+                print("found space in term")
+                from IPython import embed;embed(colors='Linux')
+            pre,post=self._hsetKeys(term)
+            bdata=self.db.hget(pre,post)
+            if bdata!=None:
+                res.append(j.tools.numtools.bin_to_listint(bdata))
+
+        #now need to find the ones which exist in all items of res (need to find the union)
+        
+        res=[set(item) for item in res]
+        res2 = reduce(set.intersection, res)
+
+        return res2
+
+                
+    
+
+
+
+    def test(self):
+        """
+        js9 'j.data.bcdb.test()'
+
+        """
+
+        import nltk
+        # nltk.download('brown')
+        from nltk.corpus import brown
+
+        j.tools.timer.start("perftest_index")
+        nr=100
+        namespace="myns"
+        
+        words= brown.words()[:nr*10]
+
+        def getword(words):
+            found=""
+            while len(found)<5:
+                picknr=j.data.idgenerator.generateRandomInt(0,len(words1))
+                found=words[picknr]
+            return found
+
+
+        pos=0
+        name1=""
+        words1=[]
+        for i in range(nr):
+            rand=j.data.idgenerator.generateRandomInt(1,8)#to create sentences
+            toindex=""
+            for i in range(rand):
+                picknr=j.data.idgenerator.generateRandomInt(0,nr*10-1)
+                word=words[picknr]
+                words1.append(word)
+                toindex+=" %s"%word
+
+            toindex=toindex.strip()
+            self.index_add_term(toindex,i)
+
+        j.tools.timer.stop(nr)
+
+        w1=getword(words1)
+        w2=getword(words1)
+        w3=getword(words1)
+        res=self.find(namespace,[w1])
+        res1=self.find(namespace,[w2,w3])
+
+
+        from IPython import embed;embed(colors='Linux')

JumpScale9Lib/data/capnp3/ModelBase3.py
@@ -1,0 +1,238 @@
+from js9 import j
+from collections import OrderedDict
+
+
+class ModelBase3():
+
+    def __init__(self, key="", new=False, collection=None):
+
+        self._propnames = []
+        self.collection = collection
+        self.logger = collection.logger
+
+        self._key = ""
+
+        self.dbobj = None
+        self.changed = False
+        self._subobjects = {}
+
+        if j.data.types.bytes.check(key):
+            key = key.decode()
+
+        # if key != "":
+        #     if len(key) != 16 and len(key) != 32 and len(key) != 64:
+        #         raise j.exceptions.Input("Key needs to be length 16,32,64")
+
+        if new:
+            self.dbobj = self.collection.capnp_schema.new_message()
+            self._post_init()
+            if key != "":
+                self._key = key
+        elif key != "":
+            # will get from db
+            if self.collection._db.exists(key):
+                self.load(key=key)
+                self._key = key
+            else:
+                raise j.exceptions.Input(message="Cannot find object:%s!%s" % (
+                    self.collection.category, key), level=1, source="", tags="", msgpub="")
+        else:
+            raise j.exceptions.Input(message="key cannot be empty when no new obj is asked for.",
+                                     level=1, source="", tags="", msgpub="")
+
+    @property
+    def key(self):
+        if self._key is None or self._key == "":
+            self._key = self._generate_key()
+        return self._key
+
+    @key.setter
+    def key(self, value):
+        if j.data.types.bytes.check(value):
+            value = value.decode()
+        self._key = value
+
+    def _post_init(self):
+        pass
+
+    def _pre_save(self):
+        # needs to be implemented see e.g. ActorModel
+        pass
+
+    def _generate_key(self):
+        # return a unique key to be used in db (std the key but can be overriden)
+        return j.data.hash.md5_string(j.data.idgenerator.generateGUID())
+
+    def index(self):
+        # put indexes in db as specified
+        if self.collection!=None:
+            self.collection._index.index({self.dbobj.name: self.key})
+
+    def load(self, key):
+        if self.collection._db.inMem:
+            self.dbobj = self.collection._db.get(key)
+        else:
+            buff = self.collection._db.get(key)
+            self.dbobj = self.collection.capnp_schema.from_bytes(buff, builder=True)
+
+    # TODO: *2 would be nice that this works, but can't get it to work, something recursive
+    # def __setattr__(self, attr, val):
+    #     if attr in ["_propnames", "_subobjects", "dbobj", "_capnp_schema"]:
+    #         self.__dict__[attr] = val
+    #         print("SETATTRBASE:%s" % attr)
+    #         # return ModelBase.__setattr__(self, attr, val)
+    #
+    #     print("SETATTR:%s" % attr)
+    #     if attr in self._propnames:
+    #         print("1%s" % attr)
+    #         # TODO: is there no more clean way?
+    #         dbobj = self._subobjects
+    #         print(2)
+    #         exec("dbobj.%s=%s" % (attr, val))
+    #         print(3)
+    #         #
+    #     else:
+    #         raise j.exceptions.Input(message="Cannot set attr:%s in %s" %
+    #                                  (attr, self), level=1, source="", tags="", msgpub="")
+
+    # def __dir__(self):
+    #     propnames = ["key", "index", "load", "_post_init", "_pre_save", "_generate_key", "save", "logger",
+    #                  "dictFiltered", "reSerialize", "dictJson", "raiseError", "addSubItem", "_listAddRemoveItem",
+    #                  "logger", "_capnp_schema", "_category", "_db", "_index", "_key", "dbobj", "changed", "_subobjects"]
+    #     return propnames + self._propnames
+
+    def reSerialize(self):
+        for key in list(self._subobjects.keys()):
+            prop = self.__dict__["list_%s" % key]
+            dbobjprop = eval("self.dbobj.%s" % key)
+            if len(dbobjprop) != 0:
+                raise RuntimeError("bug, dbobj prop should be empty, means we didn't reserialize properly")
+            if prop is not None and len(prop) > 0:
+                # init the subobj, iterate over all the items we have & insert them
+                subobj = self.dbobj.init(key, len(prop))
+                for x in range(0, len(prop)):
+                    subobj[x] = prop[x]
+
+            self._subobjects.pop(key)
+            self.__dict__.pop("list_%s" % key)
+
+    def save(self):
+        self.reSerialize()
+        self._pre_save()
+        if self.collection._db.inMem:
+            self.collection._db.db[self.key] = self.dbobj
+        else:
+            # no need to store when in mem because we are the object which does not have to be serialized
+            # so this one stores when not mem
+            buff = self.dbobj.to_bytes()
+            if hasattr(self.dbobj, 'clear_write_flag'):
+                self.dbobj.clear_write_flag()
+            self.collection._db.set(self.key, buff)
+        self.index()
+
+    def to_dict(self):
+        self.reSerialize()
+        d = self.dbobj.to_dict()
+        d['key'] = self.key
+        return d
+
+    @property
+    def dictFiltered(self):
+        """
+        remove items from obj which cannot be serialized to json or not relevant in dict
+        """
+        # made to be overruled
+        return self.to_dict()
+
+    @dictFiltered.setter
+    def dictFiltered(self, ddict):
+        """
+        """
+        if "key" in ddict:
+            self.key = ddict[key]
+        self.dbobj = self.collection.capnp_schema.new_message(**ddict)
+
+    @property
+    def dictJson(self):
+        ddict2 = OrderedDict(self.dictFiltered)
+        return j.data.serializer.json.dumps(ddict2, sort_keys=True, indent=True)
+
+    def raiseError(self, msg):
+        msg = "Error in dbobj:%s (%s)\n%s" % (self._category, self.key, msg)
+        raise j.exceptions.Input(message=msg, level=1, source="", tags="", msgpub="")
+
+    def updateSubItem(self, name, keys, data):
+        keys = keys or []
+        if not isinstance(keys, list):
+            keys = [keys]
+        self._listAddRemoveItem(name)
+        existing = self.__dict__['list_%s' % name]
+        for idx, item in enumerate(existing):
+            match = True
+            for key in keys:
+                if item.to_dict()[key] != data.to_dict()[key]:
+                    match = False
+            if keys and match:
+                existing.pop(idx)
+                break
+        self.addSubItem(name, data)
+
+    def addDistinctSubItem(self, name, data):
+        self._listAddRemoveItem(name=name)
+        for item in self.__dict__["list_%s" % name]:
+            if item.to_dict() == data.to_dict():
+                return
+        self.__dict__["list_%s" % name].append(data)
+
+    def addSubItem(self, name, data):
+        """
+        @param data is string or object first retrieved by self.collection.list_$name_constructor(**args)
+        can also directly add them to self.list_$name.append(self.collection.list_$name_constructor(**args)) if it already exists
+        """
+        self._listAddRemoveItem(name=name)
+        self.__dict__["list_%s" % name].append(data)
+
+    def initSubItem(self, name):
+        self._listAddRemoveItem(name=name)
+
+    def deleteSubItem(self, name, pos):
+        """
+        @param pos is the position in the list
+        """
+        self._listAddRemoveItem(name=name)
+        self.__dict__["list_%s" % name].pop(pos)
+        self.reSerialize()
+
+    def _listAddRemoveItem(self, name):
+        """
+        if you want to change size of a list on obj use this method
+        capnp doesn't allow modification of lists, so when we want to change size of a list then we need to reSerialize
+        and put content of a list in a python list of dicts
+        we then re-serialize and leave the subobject empty untill we know that we are at point we need to save the object
+        when we save we populate the subobject so we get a nicely created capnp message
+        """
+        if name in self._subobjects:
+            # means we are already prepared
+            return
+        prop = eval("self.dbobj.%s" % name)
+        if len(prop) == 0:
+            self.__dict__["list_%s" % name] = []
+        else:
+            try:
+                self.__dict__["list_%s" % name] = [item.copy() for item in prop]
+            except BaseException:                # means is not an object can be e.g. a string
+                self.__dict__["list_%s" % name] = [item for item in prop]
+
+        # empty the dbobj list
+        exec("self.dbobj.%s=[]" % name)
+
+        self._subobjects[name] = True
+        self.changed = True
+
+    def __repr__(self):
+        out = "key:%s\n" % self.key
+        out += self.dictJson
+        return out
+
+    __str__ = __repr__
+

JumpScale9Lib/data/capnp3/ModelBaseCollection3.py
@@ -1,0 +1,174 @@
+from js9 import j
+from collections import OrderedDict
+
+
+def getText(text):
+    return str(object=text)
+
+
+def getInt(nr):
+    return int(nr)
+
+
+class ModelBaseCollection3:
+    """
+    This class represent a collection
+    It's used to list/find/create new Instance of Model objects
+
+    """
+
+    def __init__(self, schema, category, namespace=None, modelBaseClass=None, db=None, indexDb=None):
+        """
+        @param schema: object created by the capnp librairies after it load a .capnp file.
+        example :
+            import capnp
+            # load the .capnp file
+            import model_capnp as ModelCapnp
+            # pass this to the constructor.
+            ModelCapnp.MyStruct
+        @param category str: category of the model. need to be the same as the category of the single model class
+        @param namespace: namespace used to store these object in key-value store
+        @param modelBaseClass: important to pass the class not the object. Class used to create instance of this category.
+                               Need to inherits from JumpScale9.data.capnp.ModelBase.ModelBalse
+        @param db: connection object to the key-value store
+        @param indexDb: connection object to the key-value store used for indexing
+        """
+
+        self.category = category
+        self.namespace = namespace if namespace else category
+        self.capnp_schema = schema
+
+        self.propnames = [item for item in self.capnp_schema.schema.fields.keys()]
+
+        self._listConstructors = {}
+
+        for field in self.capnp_schema.schema.fields_list:
+            try:
+                str(field.schema)
+            except BaseException:
+                continue
+
+            if "List" in str(field.schema):
+                slottype = str(field.proto.slot.type).split("(")[-1]
+                if slottype.startswith("text"):
+                    # is text
+                    self._listConstructors[field.proto.name] = getText
+                elif slottype.startswith("uint"):
+                    # is text
+                    self._listConstructors[field.proto.name] = getInt
+                else:
+                    subTypeName = str(field.schema.elementType).split(':')[-1][:-1].split('.')[-1]
+                    # subTypeName = str(field.schema.elementType).split(".")[-1].split(">")[0]
+                    try:
+                        self._listConstructors[field.proto.name] = eval(
+                            "self.capnp_schema.%s.new_message" % subTypeName)
+                    except BaseException:
+                        continue
+
+                self.__dict__["list_%s_constructor" % field.proto.name] = self._listConstructors[field.proto.name]
+
+        self._db = db if db else j.data.kvs.getMemoryStore(name=self.namespace, namespace=self.namespace)
+        # for now we do index same as database
+        self._index = indexDb if indexDb else self._db
+
+        self.modelBaseClass = modelBaseClass if modelBaseClass else ModelBase
+
+        self.logger = j.logger.get("modelBase_%s" % category)
+
+        self._init()
+
+    def _init(self):
+        pass
+
+    @property
+    def indexDB(self):
+        return self._index
+
+    def _arraysFromArgsToString(self, names, args):
+        """
+        will translate arrays or non arrays to string in format
+            '$name1','$name2'
+
+        @param names tells us which items from the args which needs to be processed
+
+        items to process in args are:
+            can be "a,b,c"
+            can be "'a','b','c'"
+            can be ["a","b","c"]
+            can be "a"
+
+        @param args is dict with arguments to check
+        """
+        for name in names:
+            if name in args:
+                if j.data.types.string.check(args[name]) and "," in args[name]:
+                    args[name] = [item.strip().strip("'").strip() for item in args[name].split(",")]
+                elif not j.data.types.list.check(args[name]):
+                    args[name] = [args[name]]
+                args[name] = ",".join(["'%s'" % item for item in args[name]])
+        return args
+
+    @property
+    def objType(self):
+        return self.capnp_schema.schema.node.displayName
+
+    def new(self, key=""):
+        model = self.modelBaseClass(key=key, new=True, collection=self)
+        return model
+
+    def exists(self, key):
+        return self._db.exists(key)
+
+    def get(self, key, autoCreate=False):
+        if self._db.inMem:
+            if key in self._db.db:
+                model = self._db.db[key]
+            else:
+                if autoCreate:
+                    return self.new(key=key)
+                else:
+                    raise j.exceptions.Input(message="Could not find key:%s for model:%s" %
+                                             (key, self.category), level=1, source="", tags="", msgpub="")
+        else:
+
+            model = self.modelBaseClass(
+                key=key,
+                new=autoCreate,
+                collection=self)
+        return model
+
+    def list(self, **kwargs):
+        """
+        List all keys of a index
+        """
+        res = []
+
+        return res
+
+    def find(self, **kwargs):
+        """
+        finds all entries matching kwargs
+
+        e.g.
+        email="reem@greenitglobe.com", name="reem"
+        """
+        res = []
+        for key in self.list(**kwargs):
+            res.append(self.get(key))
+        return res
+
+    def destroy(self):
+        self._db.destroy()
+        self._index.destroy()
+
+    def lookupGet(self, name, key):
+        return self._index.lookupGet(name, key)
+
+    def lookupSet(self, name, key, fkey):
+        """
+        @param fkey is foreign key
+        """
+        return self._index.lookupSet(name, key, fkey)
+
+    def lookupDestroy(self, name):
+        return self._index.lookupDestroy(name)

commit ba5b11e108800a9f3e59d7cc488e5172eb52f78d

fix circular import in capnp module
bug introduced by:
https://github.com/Jumpscale/lib9/commit/f2e708a4075a046d72d03b9239908089cbb0a166

JumpScale9Lib/data/capnp3/Capnp.py
@@ -3,9 +3,9 @@ import sys
 import capnp
 from collections import OrderedDict
 import capnp
-from .ModelBase import ModelBase
-from .ModelBase import ModelBaseWithData
-from .ModelBase import ModelBaseCollection
+
+from .ModelBaseCollection3 import ModelBaseCollection3
+from .ModelBaseData3 import ModelBaseData
 
 
 class Tools():
@@ -59,7 +59,7 @@ class Capnp:
         return ModelBaseCollection
 
     def getModelCollection(self, schema, category, namespace=None, modelBaseClass=None,
-                           modelBaseCollectionClass=None, db=None,indexDb=None):
+                           modelBaseCollectionClass=None, db=None, indexDb=None):
         """
         @param schema is capnp_schema
 
@@ -125,7 +125,7 @@ class Capnp:
 
             }
             """ % j.data.idgenerator.generateCapnpID()
-            
+
         schemas = self._getSchemas(schemaInText)
         schema = eval("schemas.%s" % name)
         return schema
@@ -226,7 +226,6 @@ class Capnp:
         # dummy test, not used later
         obj = self.getObj(capnpschema, name="Issue")
         obj.state = "ok"
-        
 
         # now we just get the capnp schema for this object
         schema = self.getSchemaFromText(capnpschema, name="Issue")
@@ -252,7 +251,8 @@ class Capnp:
         print("population in %.2fs" % (end_populate - start))
         print("find in %.2fs" % (end_find - end_populate))
 
-        from IPython import embed;embed(colors='Linux')
+        from IPython import embed
+        embed(colors='Linux')
 
     def testWithRedis(self):
         capnpschema = '''

JumpScale9Lib/data/capnp3/ModelBaseData3.py
@@ -1,9 +1,10 @@
 from js9 import j
 # from collections import OrderedDict
 
-from .ModelBase import ModelBase
+from .ModelBase3 import ModelBase3
 
-class ModelBaseData(ModelBase):
+
+class ModelBaseData(ModelBase3):
 
     def __init__(self, key="", new=False, collection=None):
         super().__init__(key=key, new=new, collection=collection)
@@ -35,4 +36,3 @@ def getText(text):
 
 def getInt(nr):
     return int(nr)
-