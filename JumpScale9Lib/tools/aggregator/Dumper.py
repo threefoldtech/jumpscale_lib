f795a6dcbd0537bb9bca12a883f91013978a7ba3
Jo De Boeck <deboeck.jo@gmail.com>
2018-03-06 09:45:38 +0200

    Bring back influxdimper under new name
    This also make its rescan the network on a specified interval
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- JumpScale9Lib/tools/aggregator/Dumper.py
+++ JumpScale9Lib/tools/aggregator/Dumper.py
@@ -3,6 +3,7 @@ from .NetworkScanner import NetworkScanner
 import multiprocessing
 import time
 import logging
+from queue import Empty
 
 
 NUM_WORKERS = 4
@@ -10,35 +11,52 @@ NUM_WORKERS = 4
 
 class BaseDumper:
 
-    def __init__(self, cidr, ports=[6379]):
+    def __init__(self, cidr, ports=[6379], scaninterval=300):
         logging.root.setLevel(logging.INFO)
 
         self._cidr = cidr
-        scanner = NetworkScanner(cidr, ports)
-        self._candidates = scanner.scan()
+        self._scaninterval = scaninterval
+        self._scanner = NetworkScanner(cidr, ports)
+        self._circulation = set()
+
+    def update_queue(self, queue):
+        candidates = self._scanner.scan()
+        for ip, ports in candidates.items():
+            for port in ports:
+                item = (ip, port)
+                if item not in self._circulation:
+                    self._circulation.add(item)
+                    queue.put_nowait((ip, port))
 
     def start(self, workers=NUM_WORKERS):
         manager = multiprocessing.Manager()
         queue = manager.Queue()
-        for ip, ports in self.candidates.items():
-            for port in ports:
-                queue.put_nowait((ip, port))
+        errorqueue = manager.Queue()
+        self.update_queue(queue)
 
         pool = multiprocessing.Pool(workers)
+        scantime = time.time()
 
         while True:
-            ip, port = queue.get()
-            pool.apply_async(self._process, (ip, port, queue))
+            if scantime + self._scaninterval < time.time():
+                self.update_queue(queue)
+                scantime = time.time()
+            while not errorqueue.empty():
+                erroritem = errorqueue.get()
+                if erroritem in self._circulation:
+                    self._circulation.remove(erroritem)
+            try:
+                ip, port = queue.get(timeout=5)
+                pool.apply_async(self._process, (ip, port, queue, errorqueue))
+            except Empty:
+                # queue is empty carry on
+                pass
 
     @property
     def cidr(self):
         return self._cidr
 
-    @property
-    def candidates(self):
-        return self._candidates
-
-    def _process(self, ip, port, queue):
+    def _process(self, ip, port, queue, errorqueue):
         redis = j.clients.redis.get(ip, port)
         now = int(time.time())
         try:
@@ -46,6 +64,7 @@ class BaseDumper:
             self.dump(redis)
         except Exception:
             logging.exception("Failed to process redis '%s:%s'" % (ip, port))
+            errorqueue.put_nowait((ip, port))
         finally:
             # workers must have some rest (1 sec) before moving to next
             # ip to process
@@ -53,7 +72,7 @@ class BaseDumper:
                 # process took very short time. Give worker time to rest
                 time.sleep(1)
 
-            queue.put_nowait((ip, port))
+        queue.put_nowait((ip, port))
 
     def dump(self, redis):
         """
