2c11cd87f28ee397dbfcad2eb9790485c657591c
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2018-03-27 00:12:33 +0200

    update user-agent and update tests

--- tests/clients/rivine/test_RivineWallet.py
+++ tests/clients/rivine/test_RivineWallet.py
@@ -3,12 +3,8 @@ Test module for RivineWallet js9 client
 """
 
 from js9 import j
-from mnemonic import Mnemonic
 from JumpScale9Lib.clients.rivine.RivineWallet import RivineWallet, SpendableKey
 
-# create a random seed
-m = Mnemonic('english')
-seed = m.generate()
 
 # use specific seed that has some funds
 seed = 'siren own oil clean often undo castle sure creek squirrel group income size boost cart picture wing cram candy dutch congress actor taxi prosper'
@@ -16,23 +12,28 @@ seed = 'siren own oil clean often undo castle sure creek squirrel group income s
 client_data = {'bc_address': 'http://185.69.166.13:2015',
                'password_': 'test123',
                'minerfee': 10,
-               'nr_keys_per_seed': 5,
+               'nr_keys_per_seed': 10,
                'seed_': seed}
 
 rivine_client = j.clients.rivine.get('mytestwallet', data=client_data)
 rivine_client.config.save()
 
-expected_unlockhashes = ['20f4c5d518839b61a2a704bd45d47e25a319df75c1fa18c01856440079d34b0b0181076ce5c2',
- 'ddd9d661b8694b1e7b36c09c211aeb400579c3ca9feff4d86d892c84fe8b10f31cae805c23e2',
- '59012f7b5a1529ce0ec0bcf641cc65da2f2cb2d8fa65b7f6b57653e9053488a9ec8057930d76',
- '0d59ae725d13d8db337dd1356c736a24aab00f8e6d64ae324516d0fd0ff00ed8ef597789cfcc',
- '87a97bb7aac48ed7f3ea857536368a68b11c227cd5fc374c3a3f5df999d1d726f2623fe5e82b']
+expected_unlockhashes = ['fa8ee5ecf2551f9a5a8cdc338ad6ef781d53929d712ebbe25b93a1ff4c89584d6a430fd60874',
+ '3e47b15d4a1d3974a326556cd127d5b86d607cb8726165bb4c9a3515a98b2ba1982a35939c7f',
+ '573cbb732e4a075c3b95103efce8dbc479cc79d2ac9ad917c0c1f5facfbfa6648366a0735080',
+ 'f65988eefa5f76d0d73f9879e91772e709faa947d1d6f03a2e36b554698594bf07738072f6bb',
+ '1c0baae330902503bbdaa945be3d8f6359c0f21bdba392046c7507992aa900ba0df185208b15',
+ '27ac47fda7f5ac7f3eeb83a29e26aeb9f82d14dd9b74d7ec277af90d2f90d8b97f8ca7b9f4a9',
+ '45df536e9ad219fcfa9b2cd295d3499e59ced97e6cbed30d81373444db01acd586e8f302d602',
+ '62c3980953d55d7670859f474aec7a8caeb1517421c0e744a6c7c8e4ad00a6db043c35d0c0e6',
+ 'dd6b2cb6098f301d6928041d58119c5c9cc62be4a6ba797c8f8a510cd3b0c5bd38bec968e62b',
+ '9d0e215e29a548bb3aca345eeffe7a47d4bb49e7ecae29d5bd548632b1829559c283fe00bf6c']
 
 # create a wallet based on the generated Seed
 # rivine_wallet = RivineWallet(seed=seed, bc_network='http://185.69.166.13:2015', nr_keys_per_seed=5)
 rivine_wallet = rivine_client.wallet
 
-actual_unlockhashes = [key for key in rivine_wallet.keys.keys()]
+actual_unlockhashes = rivine_wallet.addresses
 
 assert set(expected_unlockhashes) == set(
     actual_unlockhashes), "Unlockhashes do not match"
@@ -146,64 +147,23 @@ expected_address_info =\
       'signature': '641ZU6EX1Q40TyAACb+V/FBysza+cd+1DmOavQAkfTbLOEZWR25ePUuU1HEnHGyndEvJUXp4FTgcfa7dk23fAg==',
       'timelock': 0}]}}]}
 
-# import json
-# expected_address_info = json.loads(str(expected_address_info))
+
 address = '20f4c5d518839b61a2a704bd45d47e25a319df75c1fa18c01856440079d34b0b0181076ce5c2'
 actual_address_info = rivine_wallet.check_address(address=address)
-# import pdb; pdb.set_trace()
 assert actual_address_info == expected_address_info, "Expected address info is not the same as check_address found"
 
 
-# import copy
-# original_keys = copy.deepcopy(rivine_wallet._keys)
-# override the keys attribute in the wallet to use our testing address
-# rivine_wallet._keys = {address: SpendableKey(m.to_seed(seed))}
-# after overriding the keys attributes we can try to sync the wallet
+#sync the wallet
 rivine_wallet.sync_wallet()
 
-expected_unspent_coins_outputs = {'b7e412be3cf4636d4d65056d270f8c0097679b4dd3ffe355fbbc0ca57eb20a8c': {'unlockhash': '20f4c5d518839b61a2a704bd45d47e25a319df75c1fa18c01856440079d34b0b0181076ce5c2',
-  'value': '10000000000000000000000000000'}}
-
-assert expected_unspent_coins_outputs == rivine_wallet.unspent_coins_outputs, "Unexpected unspent coins outputs"
-
-
-# set back the original keys
-# import pdb; pdb.set_trace()
-# rivine_wallet._keys = original_keys
+# create transaction
 recipient = 'e5bd83a85e263817e2040054064575066874ee45a7697facca7a2721d4792af374ea35f549a1'
-# transacton = rivine_wallet.create_transaction(amount=10, recipient=recipient)
 transaction = rivine_wallet.create_transaction(amount=500, recipient=recipient)
 
-expected_transaction_json = {'arbitrarydata': '',
- 'blockstakeinputs': '',
- 'blockstakeoutputs': '',
- 'coininputs': [{'parentid': 'b7e412be3cf4636d4d65056d270f8c0097679b4dd3ffe355fbbc0ca57eb20a8c',
-   'unlockconditions': {'publickeys': [{'algorithm': 'Ed25519',
-      'key': 'qdsbI+5eEQwM/9QzBwdP7TDESR2syXRQ2jhAhyx1hPA='}],
-    'signaturesrequired': 1,
-    'timelock': 0}}],
- 'coinoutputs': [{'unlockhash': 'e5bd83a85e263817e2040054064575066874ee45a7697facca7a2721d4792af374ea35f549a1',
-   'value': '500'},
-  {'unlockhash': 'ddd9d661b8694b1e7b36c09c211aeb400579c3ca9feff4d86d892c84fe8b10f31cae805c23e2',
-   'value': '9999999999999999999999999490'}],
- 'minerfees': ['10'],
- 'transactionsignatures': [{'coveredfields': {'arbitrarydata': '',
-    'blockstakeinputs': '',
-    'blockstakeoutputs': '',
-    'coininputs': '',
-    'coinoutputs': '',
-    'minerfees': '',
-    'transactionsignatures': '',
-    'wholetransaction': True},
-   'parentid': 'b7e412be3cf4636d4d65056d270f8c0097679b4dd3ffe355fbbc0ca57eb20a8c',
-   'publickeyindex': 0,
-   'signature': 'EDqeePftyr/d0QF1OGI4AHk1sHe+ynfGnR1V0OZR+xui/SO0EYHVXbaVZ8QV5NElaNuQ1XHoA8S/rIL/vTBPBA==',
-   'timelock': 0}]}
 
-assert transaction.json == expected_transaction_json, "Wrong transaction json was generated"
 print('Transaction created!')
 
-rivine_wallet.commit_transaction(transaction) 
+rivine_wallet.commit_transaction(transaction)
 print('Transaction pushed to chain')
 
 # create transaction with custom data
@@ -211,5 +171,5 @@ custom_data = bytearray("hello from Dresden", encoding='utf-8')
 
 transaction = rivine_wallet.create_transaction(amount=500, recipient=recipient, custom_data=custom_data)
 
-rivine_wallet.commit_transaction(transaction) 
+rivine_wallet.commit_transaction(transaction)
 print('Transaction pushed to chain')
