threefoldtech/jumpscale_lib

commit e10809b7eb6d2d21c516dcc979ae742a546db299

Client for cgroups (#321)
* Client for cgroups

* fix doc string

JumpScale9Lib/clients/zero_os/BaseClient.py
@@ -1,0 +1,220 @@
+import json
+from . import typchk
+from .FilesystemManager import FilesystemManager
+from .IPManager import IPManager
+from .JobManager import JobManager
+from .ProcessManager import ProcessManager
+from .InfoManager import InfoManager
+from .Response import ResultError
+
+class BaseClient():
+    _system_chk = typchk.Checker({
+        'name': str,
+        'args': [str],
+        'dir': str,
+        'stdin': str,
+        'env': typchk.Or(typchk.Map(str, str), typchk.IsNone()),
+    })
+
+    _bash_chk = typchk.Checker({
+        'stdin': str,
+        'script': str,
+    })
+
+    def __init__(self, timeout=None):
+
+        if timeout is None:
+            self.timeout = DefaultTimeout
+        else:
+            self.timeout = timeout
+        self._info = InfoManager(self)
+        self._job = JobManager(self)
+        self._process = ProcessManager(self)
+        self._filesystem = FilesystemManager(self)
+        self._ip = IPManager(self)
+
+    @property
+    def info(self):
+        """
+        info manager
+        :return:
+        """
+        return self._info
+
+    @property
+    def job(self):
+        """
+        job manager
+        :return:
+        """
+        return self._job
+
+    @property
+    def process(self):
+        """
+        process manager
+        :return:
+        """
+        return self._process
+
+    @property
+    def filesystem(self):
+        """
+        filesystem manager
+        :return:
+        """
+        return self._filesystem
+
+    @property
+    def ip(self):
+        """
+        ip manager
+        :return:
+        """
+        return self._ip
+
+    def raw(self, command, arguments, queue=None, max_time=None, stream=False, tags=None, id=None):
+        """
+        Implements the low level command call, this needs to build the command structure
+        and push it on the correct queue.
+
+        :param command: Command name to execute supported by the node (ex: core.system, info.cpu, etc...)
+                        check documentation for list of built in commands
+        :param arguments: A dict of required command arguments depends on the command name.
+        :param queue: command queue (commands on the same queue are executed sequentially)
+        :param max_time: kill job server side if it exceeded this amount of seconds
+        :param stream: If True, process stdout and stderr are pushed to a special queue (stream:<id>) so
+            client can stream output
+        :param tags: job tags
+        :param id: job id. Generated if not supplied
+        :return: Response object
+        """
+        raise NotImplemented()
+
+    def sync(self, command, arguments, tags=None, id=None):
+        """
+        Same as self.raw except it do a response.get() waiting for the command execution to finish and reads the result
+        :param command: Command name to execute supported by the node (ex: core.system, info.cpu, etc...)
+                        check documentation for list of built in commands
+        :param arguments: A dict of required command arguments depends on the command name.
+        :param tags: job tags
+        :param id: job id. Generated if not supplied
+        :return: Result object
+        """
+        response = self.raw(command, arguments, tags=tags, id=id)
+
+        result = response.get()
+        if result.state != 'SUCCESS':
+            if not result.code:
+                result.code = 500
+            raise ResultError(msg='%s' % result.data, code=result.code)
+
+        return result
+
+    def json(self, command, arguments, tags=None, id=None):
+        """
+        Same as self.sync except it assumes the returned result is json, and loads the payload of the return object
+        if the returned (data) is not of level (20) an error is raised.
+        :Return: Data
+        """
+        result = self.sync(command, arguments, tags=tags, id=id)
+        if result.level != 20:
+            raise RuntimeError('invalid result level, expecting json(20) got (%d)' % result.level)
+
+        return json.loads(result.data)
+
+    def ping(self):
+        """
+        Ping a node, checking for it's availability. a Ping should never fail unless the node is not reachable
+        or not responsive.
+        :return:
+        """
+        return self.json('core.ping', {})
+
+    def system(self, command, dir='', stdin='', env=None, queue=None, max_time=None, stream=False, tags=None, id=None):
+        """
+        Execute a command
+
+        :param command:  command to execute (with its arguments) ex: `ls -l /root`
+        :param dir: CWD of command
+        :param stdin: Stdin data to feed to the command stdin
+        :param env: dict with ENV variables that will be exported to the command
+        :param id: job id. Auto generated if not defined.
+        :return:
+        """
+        parts = shlex.split(command)
+        if len(parts) == 0:
+            raise ValueError('invalid command')
+
+        args = {
+            'name': parts[0],
+            'args': parts[1:],
+            'dir': dir,
+            'stdin': stdin,
+            'env': env,
+        }
+
+        self._system_chk.check(args)
+        response = self.raw(command='core.system', arguments=args,
+                            queue=queue, max_time=max_time, stream=stream, tags=tags, id=id)
+
+        return response
+
+    def bash(self, script, stdin='', queue=None, max_time=None, stream=False, tags=None, id=None):
+        """
+        Execute a bash script, or run a process inside a bash shell.
+
+        :param script: Script to execute (can be multiline script)
+        :param stdin: Stdin data to feed to the script
+        :param id: job id. Auto generated if not defined.
+        :return:
+        """
+        args = {
+            'script': script,
+            'stdin': stdin,
+        }
+        self._bash_chk.check(args)
+        response = self.raw(command='bash', arguments=args,
+                            queue=queue, max_time=max_time, stream=stream, tags=tags, id=id)
+
+        return response
+
+    def subscribe(self, job, id=None):
+        """
+        Subscribes to job logs. It return the subscribe Response object which you will need to call .stream() on
+        to read the output stream of this job.
+
+        Calling subscribe multiple times will cause different subscriptions on the same job, each subscription will
+        have a copy of this job streams.
+
+        Note: killing the subscription job will not affect this job, it will also not cause unsubscripe from this stream
+        the subscriptions will die automatically once this job exits.
+
+        example:
+            job = client.system('long running job')
+            subscription = client.subscribe(job.id)
+
+            subscription.stream() # this will print directly on stdout/stderr check stream docs for more details.
+
+        hint: u can give an optional id to the subscriber (otherwise a guid will be generate for you). You probably want
+        to use this in case your job watcher died, so u can hook on the stream of the current subscriber instead of creating a new one
+
+        example:
+            job = client.system('long running job')
+            subscription = client.subscribe(job.id, 'my-job-subscriber')
+
+            subscription.stream()
+
+            # process dies for any reason
+            # on next start u can simply do
+
+            subscription = client.response_for('my-job-subscriber')
+            subscription.stream()
+
+
+        :param job: the job ID to subscribe to
+        :param id: the subscriber ID (optional)
+        :return: the subscribe Job object
+        """
+        return self.raw('core.subscribe', {'id': job}, stream=True, id=id)
+

JumpScale9Lib/clients/zero_os/Client.py
@@ -9,11 +9,12 @@ import time
 from js9 import j
 
 from . import typchk
+from .BaseClient import BaseClient
 from .AggregatorManager import AggregatorManager
 from .BridgeManager import BridgeManager
 from .BtrfsManager import BtrfsManager
 from .Config import Config
-from .ContainerManager import BaseClient, ContainerManager
+from .ContainerManager import ContainerManager
 from .DiskManager import DiskManager
 from .KvmManager import KvmManager
 from .LogManager import LogManager
@@ -22,6 +23,7 @@ from .Response import Response
 from .ZerotierManager import ZerotierManager
 from .WebManager import WebManager
 from .RTInfoManager import RTInfoManager
+from .CGroupManager import CGroupManager
 
 DefaultTimeout = 10  # seconds
 
@@ -69,6 +71,8 @@ class Client(BaseClient, JSConfigClientBase):
         self._jwt_expire_timestamp = 0
         self._web = WebManager(self)
         self._rtinfo = RTInfoManager(self)
+        self._cgroup = CGroupManager(self)
+
 
     @property
     def _redis(self):
@@ -201,6 +205,13 @@ class Client(BaseClient, JSConfigClientBase):
         :return:
         """
         return self._rtinfo
+
+    @property
+    def cgroup(self):
+        """
+        Cgroup manager
+        """
+        return self._cgroup
 
     def raw(self, command, arguments, queue=None, max_time=None, stream=False, tags=None, id=None):
         """

JumpScale9Lib/clients/zero_os/CGroupManager.py
@@ -1,0 +1,180 @@
+from js9 import j
+from . import typchk
+
+
+class CGroupManager:
+    _subsystem_chk = typchk.Enum('cpuset', 'memory')
+
+    _cgroup_chk = typchk.Checker({
+        'subsystem': _subsystem_chk,
+        'name': str,
+    })
+
+    _task_chk = typchk.Checker({
+        'subsystem': _subsystem_chk,
+        'name': str,
+        'pid': int,
+    })
+
+    _memory_spec = typchk.Checker({
+        'name': str,
+        'mem': int,
+        'swap': int,
+    })
+
+    _cpuset_spec = typchk.Checker({
+        'name': str,
+        'cpus': typchk.Or(typchk.IsNone(), str),
+        'mems': typchk.Or(typchk.IsNone(), str),
+    })
+
+    def __init__(self, client):
+        self._client = client
+
+    def list(self):
+        """
+        List all cgroups names grouped by the cgroup subsystem
+        """
+        return self._client.json('cgroup.list', {})
+
+    def ensure(self, subsystem, name):
+        """
+        Creates a cgroup if it doesn't exist under the specified subsystem
+        and the given name
+
+        :param subsystem: the cgroup subsystem (currently support 'memory', and 'cpuset')
+        :param name: name of the cgroup to delete
+        """
+        args = {
+            'subsystem': subsystem,
+            'name': name,
+        }
+
+        self._cgroup_chk.check(args)
+        return self._client.json('cgroup.ensure', args)
+
+    def remove(self, subsystem, name):
+        """
+        Removes a cgroup by type/name
+
+        :param subsystem: the cgroup subsystem (currently support 'memory', and 'cpuset')
+        :param name: name of the cgroup to delete
+        """
+
+        args = {
+            'subsystem': subsystem,
+            'name': name,
+        }
+
+        self._cgroup_chk.check(args)
+        return self._client.json('cgroup.remove', args)
+
+    def tasks(self, subsystem, name):
+        """
+        List all tasks (PIDs) that are added to this cgroup
+
+        :param subsystem: the cgroup subsystem (currently support 'memory', and 'cpuset')
+        :param name: name of the cgroup
+        """
+
+        args = {
+            'subsystem': subsystem,
+            'name': name,
+        }
+
+        self._cgroup_chk.check(args)
+        return self._client.json('cgroup.tasks', args)
+
+    def task_add(self, subsystem, name, pid):
+        """
+        Add process (with pid) to a cgroup
+
+        :param subsystem: the cgroup subsystem (currently support 'memory', and 'cpuset')
+        :param name: name of the cgroup
+        :param pid: PID to add
+        """
+
+        args = {
+            'subsystem': subsystem,
+            'name': name,
+            'pid': pid,
+        }
+
+        self._task_chk.check(args)
+        return self._client.json('cgroup.task-add', args)
+
+    def task_remove(self, subsystem, name, pid):
+        """
+        Remove a process (with pid) from a cgroup
+
+        :param subsystem: the cgroup subsystem (currently support 'memory', and 'cpuset')
+        :param name: name of the cgroup
+        :param pid: PID to remove
+        """
+
+        args = {
+            'subsystem': subsystem,
+            'name': name,
+            'pid': pid,
+        }
+
+        self._task_chk.check(args)
+        return self._client.json('cgroup.task-remove', args)
+
+    def reset(self, subsystem, name):
+        """
+        Reset cgroup limitation to default values
+
+        :param subsystem: the cgroup subsystem (currently support 'memory', and 'cpuset')
+        :param name: name of the cgroup
+        """
+
+        args = {
+            'subsystem': subsystem,
+            'name': name,
+        }
+
+        self._cgroup_chk.check(args)
+        return self._client.json('cgroup.reset', args)
+
+    def memory(self, name, mem=0, swap=0):
+        """
+        Set/Get memory cgroup specification/limitation
+        the call to this method will always GET the current set values for both mem and swap.
+        If mem is not zero, the memory will set the memory limit to the given value, and swap to the given value (even 0)
+
+        :param mem: Set memory limit to the given value (in bytes), ignore if 0
+        :param swap: Set swap limit to the given value (in bytes) (only if mem is not zero)
+
+        :return: current memory limitation
+        """
+
+        args = {
+            'name': name,
+            'mem': mem,
+            'swap': swap,
+        }
+
+        self._memory_spec.check(args)
+        return self._client.json('cgroup.memory.spec', args)
+
+    def cpuset(self, name, cpus=None, mems=None):
+        """
+        Set/Get cpuset cgroup specification/limitation
+        the call to this method will always GET the current set values for both cpus and mems
+        If cpus, or mems is NOT NONE value it will be set as the spec for that attribute
+
+        :param cpus: Set cpus affinity limit to the given value (0, 1, 0-10, etc...)
+        :param mems: Set mems affinity limit to the given value (0, 1, 0-10, etc...)
+
+        :return: current cpuset
+        """
+
+        args = {
+            'name': name,
+            'cpus': cpus,
+            'mems': mems,
+        }
+
+        self._cpuset_spec.check(args)
+        return self._client.json('cgroup.cpuset.spec', args)

JumpScale9Lib/clients/zero_os/ContainerManager.py
@@ -1,228 +1,11 @@
 import shlex
 import json
-
+from .BaseClient import BaseClient
 from .Response import JSONResponse
-from .FilesystemManager import FilesystemManager
-from .IPManager import IPManager
-from .JobManager import JobManager
-from .ProcessManager import ProcessManager
-from .InfoManager import InfoManager
-from .Response import ResultError
+
 from . import typchk
 
 DefaultTimeout = 10  # seconds
-
-
-class BaseClient():
-    _system_chk = typchk.Checker({
-        'name': str,
-        'args': [str],
-        'dir': str,
-        'stdin': str,
-        'env': typchk.Or(typchk.Map(str, str), typchk.IsNone()),
-    })
-
-    _bash_chk = typchk.Checker({
-        'stdin': str,
-        'script': str,
-    })
-
-    def __init__(self, timeout=None):
-
-        if timeout is None:
-            self.timeout = DefaultTimeout
-        else:
-            self.timeout = timeout
-        self._info = InfoManager(self)
-        self._job = JobManager(self)
-        self._process = ProcessManager(self)
-        self._filesystem = FilesystemManager(self)
-        self._ip = IPManager(self)
-
-    @property
-    def info(self):
-        """
-        info manager
-        :return:
-        """
-        return self._info
-
-    @property
-    def job(self):
-        """
-        job manager
-        :return:
-        """
-        return self._job
-
-    @property
-    def process(self):
-        """
-        process manager
-        :return:
-        """
-        return self._process
-
-    @property
-    def filesystem(self):
-        """
-        filesystem manager
-        :return:
-        """
-        return self._filesystem
-
-    @property
-    def ip(self):
-        """
-        ip manager
-        :return:
-        """
-        return self._ip
-
-    def raw(self, command, arguments, queue=None, max_time=None, stream=False, tags=None, id=None):
-        """
-        Implements the low level command call, this needs to build the command structure
-        and push it on the correct queue.
-
-        :param command: Command name to execute supported by the node (ex: core.system, info.cpu, etc...)
-                        check documentation for list of built in commands
-        :param arguments: A dict of required command arguments depends on the command name.
-        :param queue: command queue (commands on the same queue are executed sequentially)
-        :param max_time: kill job server side if it exceeded this amount of seconds
-        :param stream: If True, process stdout and stderr are pushed to a special queue (stream:<id>) so
-            client can stream output
-        :param tags: job tags
-        :param id: job id. Generated if not supplied
-        :return: Response object
-        """
-        raise NotImplemented()
-
-    def sync(self, command, arguments, tags=None, id=None):
-        """
-        Same as self.raw except it do a response.get() waiting for the command execution to finish and reads the result
-        :param command: Command name to execute supported by the node (ex: core.system, info.cpu, etc...)
-                        check documentation for list of built in commands
-        :param arguments: A dict of required command arguments depends on the command name.
-        :param tags: job tags
-        :param id: job id. Generated if not supplied
-        :return: Result object
-        """
-        response = self.raw(command, arguments, tags=tags, id=id)
-
-        result = response.get()
-        if result.state != 'SUCCESS':
-            if not result.code:
-                result.code = 500
-            raise ResultError(msg='%s' % result.data, code=result.code)
-
-        return result
-
-    def json(self, command, arguments, tags=None, id=None):
-        """
-        Same as self.sync except it assumes the returned result is json, and loads the payload of the return object
-        if the returned (data) is not of level (20) an error is raised.
-        :Return: Data
-        """
-        result = self.sync(command, arguments, tags=tags, id=id)
-        if result.level != 20:
-            raise RuntimeError('invalid result level, expecting json(20) got (%d)' % result.level)
-
-        return json.loads(result.data)
-
-    def ping(self):
-        """
-        Ping a node, checking for it's availability. a Ping should never fail unless the node is not reachable
-        or not responsive.
-        :return:
-        """
-        return self.json('core.ping', {})
-
-    def system(self, command, dir='', stdin='', env=None, queue=None, max_time=None, stream=False, tags=None, id=None):
-        """
-        Execute a command
-
-        :param command:  command to execute (with its arguments) ex: `ls -l /root`
-        :param dir: CWD of command
-        :param stdin: Stdin data to feed to the command stdin
-        :param env: dict with ENV variables that will be exported to the command
-        :param id: job id. Auto generated if not defined.
-        :return:
-        """
-        parts = shlex.split(command)
-        if len(parts) == 0:
-            raise ValueError('invalid command')
-
-        args = {
-            'name': parts[0],
-            'args': parts[1:],
-            'dir': dir,
-            'stdin': stdin,
-            'env': env,
-        }
-
-        self._system_chk.check(args)
-        response = self.raw(command='core.system', arguments=args,
-                            queue=queue, max_time=max_time, stream=stream, tags=tags, id=id)
-
-        return response
-
-    def bash(self, script, stdin='', queue=None, max_time=None, stream=False, tags=None, id=None):
-        """
-        Execute a bash script, or run a process inside a bash shell.
-
-        :param script: Script to execute (can be multiline script)
-        :param stdin: Stdin data to feed to the script
-        :param id: job id. Auto generated if not defined.
-        :return:
-        """
-        args = {
-            'script': script,
-            'stdin': stdin,
-        }
-        self._bash_chk.check(args)
-        response = self.raw(command='bash', arguments=args,
-                            queue=queue, max_time=max_time, stream=stream, tags=tags, id=id)
-
-        return response
-
-    def subscribe(self, job, id=None):
-        """
-        Subscribes to job logs. It return the subscribe Response object which you will need to call .stream() on
-        to read the output stream of this job.
-
-        Calling subscribe multiple times will cause different subscriptions on the same job, each subscription will
-        have a copy of this job streams.
-
-        Note: killing the subscription job will not affect this job, it will also not cause unsubscripe from this stream
-        the subscriptions will die automatically once this job exits.
-
-        example:
-            job = client.system('long running job')
-            subscription = client.subscribe(job.id)
-
-            subscription.stream() # this will print directly on stdout/stderr check stream docs for more details.
-
-        hint: u can give an optional id to the subscriber (otherwise a guid will be generate for you). You probably want
-        to use this in case your job watcher died, so u can hook on the stream of the current subscriber instead of creating a new one
-
-        example:
-            job = client.system('long running job')
-            subscription = client.subscribe(job.id, 'my-job-subscriber')
-
-            subscription.stream()
-
-            # process dies for any reason
-            # on next start u can simply do
-
-            subscription = client.response_for('my-job-subscriber')
-            subscription.stream()
-
-
-        :param job: the job ID to subscribe to
-        :param id: the subscriber ID (optional)
-        :return: the subscribe Job object
-        """
-        return self.raw('core.subscribe', {'id': job}, stream=True, id=id)
 
 
 class ContainerClient(BaseClient):
@@ -354,7 +137,11 @@ class ContainerManager():
         'storage': typchk.Or(str, typchk.IsNone()),
         'name': typchk.Or(str, typchk.IsNone()),
         'identity': typchk.Or(str, typchk.IsNone()),
-        'env': typchk.Or(typchk.IsNone(), typchk.Map(str, str))
+        'env': typchk.Or(typchk.IsNone(), typchk.Map(str, str)),
+        'cgroups': typchk.Or(
+            typchk.IsNone(),
+            [typchk.Length((str,), 2, 2)], # array of (str, str) tuples i.e [(subsyste, name), ...]
+        )
     })
 
     _client_chk = typchk.Checker(
@@ -382,7 +169,11 @@ class ContainerManager():
     def __init__(self, client):
         self._client = client
 
-    def create(self, root_url, mount=None, host_network=False, nics=DefaultNetworking, port=None, hostname=None, privileged=False, storage=None, name=None, tags=None, identity=None, env=None):
+    def create(
+        self, root_url, mount=None, host_network=False, nics=DefaultNetworking, port=None,
+        hostname=None, privileged=False, storage=None, name=None, tags=None, identity=None, env=None,
+        cgroups=None,
+    ):
         """
         Creater a new container with the given root flist, mount points and
         zerotier id, and connected to the given bridges
@@ -426,6 +217,8 @@ class ContainerManager():
         :param name: Optional name for the container
         :param identity: Container Zerotier identity, Only used if at least one of the nics is of type zerotier
         :param env: a dict with the environment variables needed to be set for the container
+        :param cgroups: custom list of cgroups to apply to this container on creation. formated as [(subsystem, name), ...]
+                        please refer to the cgroup api for more detailes.
         """
 
         if nics == self.DefaultNetworking:
@@ -444,7 +237,8 @@ class ContainerManager():
             'storage': storage,
             'name': name,
             'identity': identity,
-            'env': env
+            'env': env,
+            'cgroups': cgroups,
         }
 
         # validate input

commit 37a73e932da2c0da57511e8cb8563c5b9f76002e

fix zero-os client after bug introduced in e10809b7eb6d2d21c516dcc979ae742a546db299

JumpScale9Lib/clients/zero_os/BaseClient.py
@@ -1,11 +1,16 @@
 import json
+import shlex
+
 from . import typchk
 from .FilesystemManager import FilesystemManager
+from .InfoManager import InfoManager
 from .IPManager import IPManager
 from .JobManager import JobManager
 from .ProcessManager import ProcessManager
-from .InfoManager import InfoManager
 from .Response import ResultError
+
+DefaultTimeout = 10  # seconds
+
 
 class BaseClient():
     _system_chk = typchk.Checker({
@@ -89,7 +94,7 @@ class BaseClient():
         :param id: job id. Generated if not supplied
         :return: Response object
         """
-        raise NotImplemented()
+        raise NotImplementedError()
 
     def sync(self, command, arguments, tags=None, id=None):
         """
@@ -217,4 +222,3 @@ class BaseClient():
         :return: the subscribe Job object
         """
         return self.raw('core.subscribe', {'id': job}, stream=True, id=id)
-

JumpScale9Lib/clients/zero_os/Client.py
@@ -1,18 +1,17 @@
 import json
-import logging
 import socket
+import time
 import uuid
 
 import redis
-import jwt
-import time
 from js9 import j
 
 from . import typchk
-from .BaseClient import BaseClient
 from .AggregatorManager import AggregatorManager
+from .BaseClient import BaseClient, DefaultTimeout
 from .BridgeManager import BridgeManager
 from .BtrfsManager import BtrfsManager
+from .CGroupManager import CGroupManager
 from .Config import Config
 from .ContainerManager import ContainerManager
 from .DiskManager import DiskManager
@@ -20,12 +19,9 @@ from .KvmManager import KvmManager
 from .LogManager import LogManager
 from .Nft import Nft
 from .Response import Response
-from .ZerotierManager import ZerotierManager
-from .WebManager import WebManager
 from .RTInfoManager import RTInfoManager
-from .CGroupManager import CGroupManager
-
-DefaultTimeout = 10  # seconds
+from .WebManager import WebManager
+from .ZerotierManager import ZerotierManager
 
 TEMPLATE = """
 host = "127.0.0.1"
@@ -72,7 +68,6 @@ class Client(BaseClient, JSConfigClientBase):
         self._web = WebManager(self)
         self._rtinfo = RTInfoManager(self)
         self._cgroup = CGroupManager(self)
-
 
     @property
     def _redis(self):