9b70055af51eafd8779b56461cbc857aaa331c94
BolaNasr <bola.e.nasr@gmail.com>
2018-09-23 12:14:40 +0200

    fixing typo

--- JumpscaleLib/sal_zos/traefik/TraefikFactory.py
+++ JumpscaleLib/sal_zos/traefik/TraefikFactory.py
@@ -10,10 +10,10 @@ class TraefikFactory(JSBASE):
         JSBASE.__init__(self)
 
     @staticmethod
-    def get(name, node, etcd_end_pint, etcd_watch=True):
+    def get(name, node, etcd_endpoint, etcd_watch=True):
         """
         Get sal for traefik
         Returns:
             the sal layer 
         """
-        return Traefik(name, node, etcd_end_pint, etcd_watch)
+        return Traefik(name, node, etcd_endpoint, etcd_watch)
