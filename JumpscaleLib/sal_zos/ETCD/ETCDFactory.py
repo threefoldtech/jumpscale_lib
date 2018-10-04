2183e9810e34e200153e8a197167f005a00fd9d3
serboctor <boctor.sarah@gmail.com>
2018-10-04 15:22:21 +0200

    Add docs to etcd factory

--- JumpscaleLib/sal_zos/ETCD/ETCDFactory.py
+++ JumpscaleLib/sal_zos/ETCD/ETCDFactory.py
@@ -15,7 +15,14 @@ class ETCDFactory(JSBASE):
         Get sal for etcd management in ZOS
         
         Arguments:
-            node, name, password, data_dir, zt_identity, nics, token, cluster
+            node: the node sal instance the etcd will be created on
+            name: the name of the etcd instance
+            password: password of the root user
+            data_dir: etcd data directory
+            zt_identity: zt identity of the etcd container
+            nics: nics to be attached to the etcd container
+            token: etcd cluster token
+            cluster: all the cluster members. ex: [{'name': 'etcd_one', 'address': 'http://172.22.14.232:2380'}]
         
         Returns:
             the sal layer 
