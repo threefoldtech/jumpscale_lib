aae2ba425c5ca9cbdb8529c7881fb62bcac67c02
Dinaamagdy <magdyd@greenitglobe.com>
2018-10-08 09:48:22 +0200

    add new travis files

--- tests/integration_tests/travis/jumspcale_install.sh
+++ tests/integration_tests/travis/jumspcale_install.sh
@@ -1,10 +1,13 @@
+if [ ${1} = "True" ]; then
+    echo "Jumpscale already installed in this machine."
+    exit
+fi
 # prepare the machine for jumpscale installation 
 apt-get install -y python3.5 python3.5-dev
 sudo rm -f /usr/bin/python
 sudo rm -f /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-
 
 ## install jumpscale 
 ./.travis_ci/prepare.sh
---
+++ tests/integration_tests/travis/setup_env.sh
@@ -0,0 +1,15 @@
+
+apt-get install -y git 
+curl -s https://install.zerotier.com/ | sudo bash
+mkdir sal_tests; cd sal_tests
+git clone https://github.com/threefoldtech/jumpscale_lib.git  -b ${1}
+cd jumpscale_lib
+# if [ ${1} = "True" ]; then
+#     sudo bash tests/integration_tests/travis/jumspcale_install.sh 
+# fi
+cd tests/integration_tests/
+pip3 install -r requirements.txt
+sudo ln -sf /usr/sbin/zerotier-cli /opt/bin/zerotier-cli
+sed -i -e"s/^nodeip=.*/nodeip=${2}/" config.ini
+sed -i -e"s/^zt_token=.*/zt_token=${3}/" config.ini
+#nosetests -v -s ${5} --tc-file=config.ini 
