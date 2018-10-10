fea3c05bfc8941ef8bad154319bcdb4f9d087a5e
AhmedHanafy725 <ahmed.hanafy725@gmail.com>
2018-10-10 07:54:41 +0200

    add 2 test cases for advanced zdb

--- tests/integration_tests/testcases/advanced/test01_zdb.py
+++ tests/integration_tests/testcases/advanced/test01_zdb.py
@@ -3,11 +3,12 @@ from framework.base.zdb import ZDB
 from framework.base.vms import VM
 from testcases.base_test import BaseTest
 from nose_parameterized import parameterized
+import multiprocessing
 import time
 import unittest
 import requests
 import random
-
+import subprocess
 class Vdisktest(BaseTest):
 
     @classmethod
@@ -17,6 +18,36 @@ class Vdisktest(BaseTest):
 
     def setUp(self):
         super().setUp()
+        self.log("Create zdb with default values.")
+        zdb_name = self.random_string()
+        self.zdb = self.zdb(node=self.node_sal)
+        self.zdb.data = self.set_zdb_default_data(name=zdb_name)
+        self.zdb.install()
+        self.zdbs.append(self.zdb)
+        
+        self.log("Create disk [D1] with default data")
+        self.disk = Vdisk(node=self.node_sal, zdb=self.zdb.zerodb_sal)
+        self.disk.data = self.set_vdisk_default_data()
+        self.mount_path = '/mnt/{}'.format(self.random_string())
+        self.disk.data['mountPoint'] = self.mount_path
+        self.disk.data['filesystem'] = 'ext4'
+        self.disk.install()
+
+        self.log("Create vm [VM1] with default values, should succeed.")
+        self.vm = self.vm(node=self.node_sal)
+        self.vm.data = self.set_vm_default_values(os_type="ubuntu")
+
+        self.log("Update default data by adding type default nics")
+        network_name = self.random_string()
+        nics = {'nics': [{'name': network_name, 'type': 'default'}]}  
+        self.vm.data = self.update_default_data(self.vm.data, nics)
+        self.vm.generate_vm_sal()
+
+        self.log(" Add zerotier network to VM1, should succeed.")
+        self.vm.add_zerotier_nics(network=self.zt_network)
+        
+        self.log("Attach disk [D1] to vm [VM1].")
+        self.vm.add_disk(self.disk.disk)
     
     def tearDown(self):
         self.log('tear down vms')
@@ -31,9 +62,115 @@ class Vdisktest(BaseTest):
                 zdb.namespace_delete(namespace.name)
             self.node_sal.client.container.terminate(zdb.zerodb_sal.container.id)
         self.zdbs.clear()
+        
 
-    def test001_attach_vdisk_to_vm(self):
-        """ SAL-026 create vdisk and attach it to vm.
+    def read_write_file(self, mount_path, guest_port, vm_zt_ip, i, result_dict):
+        self.log("Download specific file with size 10M to vm[VM1].")
+        file_name = self.random_string()
+        cmd = 'cd {}; wget https://github.com/AhmedHanafy725/test/raw/master/test_file -O {}'.format(mount_path, file_name)
+        self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd=cmd)
+
+        self.log("Get file md5 of the downloaded file.")
+        cmd = 'cd {}; md5sum {}'.format(mount_path, file_name)
+        result1 = self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd=cmd)
+        result1 = result1[:result1.find(' ')]
+
+        self.log("Download this file from [VM1] to the test machine through the server[S1].")
+        cmd = 'wget http://{}:{}/{}'.format(vm_zt_ip, guest_port, file_name)
+        response = subprocess.run(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.assertFalse(response.returncode)
+
+        self.log("Get file md5 of the downloaded file.")
+        cmd = 'md5sum {}'.format(file_name)
+        response = subprocess.run(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result2 = response.stdout.strip()
+        result2 = result2[:result2.find(' ')]
+
+        self.log("remove downloaded files")
+        cmd = 'rm -f  {}'.format(file_name)
+        response = subprocess.run(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.assertFalse(response.returncode)
+
+        result_dict[i] = (result1, result2)
+
+    def test001_read_and_write_file_from_vdisk(self):
+        """ SAL-039 create vdisk and attach it to vm.
+
+        **Test Scenario:**
+
+        #. Create zdb with default data.
+        #. Create disk [D1] with default data.
+        #. Create vm [VM1] with default values, should succeed.
+        #. Add zerotier network to VM1, should succeed.
+        #. Attach disk [D1] to vm [VM1].
+        #. Add port to create server on it.
+        #. Create server[S1], should succeed.
+        #. Do this steps five times using multiprocessing:
+            #. Download specific file with size 10M to vm[VM1].
+            #. Get file md5 of the downloaded file.
+            #. Download this file from [VM1] to the test machine through the server[S1].
+            #. Get file md5 of the downloaded file.
+        #. Download the same to the test machine.
+        #. Check md5 of the those three files, should be the same.
+
+        """
+        self.log("Add port to create server on it.")
+        port_name = self.random_string()   
+        host_port = random.randint(3000, 4000)
+        guest_port = random.randint(5000, 6000)
+        self.vm.add_port(name=port_name, source=host_port, target=guest_port)
+
+        self.log("deploy vm [VM1].")
+        self.vm.install()
+        self.vms.append(self.vm.info()['uuid'])
+        
+        self.log("get ztIdentity and vm ip")
+        ztIdentity = self.vm.data["ztIdentity"]
+        vm_zt_ip = self.get_zerotier_ip(ztIdentity)
+
+        self.log("create server on the vm at port {}.".format(guest_port))
+        cmd = 'cd {}; python3 -m http.server {} &> /tmp/server.log &'.format(self.mount_path, guest_port)
+        self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd=cmd)
+        time.sleep(10)
+        
+        self.log("Create dict to get data from multiprocess")
+        manager = multiprocessing.Manager()
+        result_dict = manager.dict()
+        jobs = []
+        
+        for i in range(5):
+            p = multiprocessing.Process(target=self.read_write_file, args=(self.mount_path, guest_port, vm_zt_ip, i, result_dict))
+            jobs.append(p)
+            p.start()
+
+        self.log("wait till all processes finish")
+        for proc in jobs:
+            proc.join()
+        
+        self.log("Download the same to the test machine.")
+        file_name2 = self.random_string()
+        cmd = 'wget https://github.com/AhmedHanafy725/test/raw/master/test_file -O {}'.format(file_name2)
+        response = subprocess.run(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.assertFalse(response.returncode)
+
+        cmd = 'md5sum {}'.format(file_name2)
+        response = subprocess.run(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = response.stdout.strip()
+        result = result[:result.find(' ')]
+        
+        self.log("remove downoaded file")
+        cmd = 'rm -f {}'.format(file_name2)
+        response = subprocess.run(cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.assertFalse(response.returncode)
+
+        self.log("Check md5 of the those three files, should be the same")
+        for i in range(5):
+            self.assertEqual(result_dict[i][0], result)
+            self.assertEqual(result_dict[i][1], result)
+
+    @parameterized.expand(['namespace', 'zdb'])    
+    def test002_delete_namespace_or_zdb_of_vdisk(self, delete):
+        """ SAL-040 create vdisk and attach it to vm.
 
         **Test Scenario:**
 
@@ -41,97 +178,32 @@ class Vdisktest(BaseTest):
         #. Create vm [VM1] with default values, should succeed.
         #. Add zerotier network to VM1, should succeed.
         #. Attach disk [D1] to vm [VM1].
-        #. deploy vm [VM1].
-        #. Check that disk [D1] is attached to vm [VM1], should succeed.
-        #. Remove the disk[D1] from the vm ,should succeed.
-
+        #. Try to write on disk mounting point, should succeed.
+        #. Delete (namespace/zdb) and try to write again, should fail.
         """
-        self.log("Create zdb with default values.")
-        zdb_name = self.random_string()
-        zdb = self.zdb(node=self.node_sal)
-        zdb.data = self.set_zdb_default_data(name=zdb_name)
-        zdb.install()
-        self.zdbs.append(zdb)
-
-        self.log("Create disk [D1] with default data")
-        disk = Vdisk(node=self.node_sal, zdb=zdb.zerodb_sal)
-        disk.data = self.set_vdisk_default_data()
-        disk.install()
-
-        self.log("Create vm [VM1] with default values, should succeed.")
-        vm = self.vm(node=self.node_sal)
-        vm.data = self.set_vm_default_values(os_type="ubuntu")
-        vm.generate_vm_sal()
-
-        self.log(" Add zerotier network to VM1, should succeed.")
-        vm.add_zerotier_nics(network=self.zt_network)
-        
-        self.log("Attach disk [D1] to vm [VM1].")
-        vm.add_disk(disk.disk, mountpoint='/mnt/hamada', filesystem='ext4', label='sba7o')
         self.log("deploy vm [VM1].")
-        vm.install()
-
-        ztIdentity = vm.data["ztIdentity"]
+        self.vm.install()
+        self.vms.append(self.vm.info()['uuid'])
+        ztIdentity = self.vm.data["ztIdentity"]
         vm_zt_ip = self.get_zerotier_ip(ztIdentity)
-        result = self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd='pwd')
-        self.assertEqual(result, '/root')
-
-        import ipdb; ipdb.set_trace()
-        self.vms.append(vm.info()['uuid'])
-        self.zdbs.append(zdb)
-
         
+        self.log("Try to write on disk mounting point, should succeed.")
+        file_name = self.random_string()
+        content = self.random_string()
+        cmd = 'echo {} > {}/{}'.format(content, self.mount_path, file_name)
+        result = self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd=cmd)
 
-    def test002_attach_vdisk_to_vm(self):
-        """ SAL-026 create vdisk and attach it to vm.
+        cmd = 'cat {}/{}'.format(self.mount_path, file_name)
+        result = self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd=cmd)
+        self.assertEqual(result, content)
 
-        **Test Scenario:**
+        self.log("Delete (namespace/zdb) and try to write again, should fail.")
+        if delete == 'zdb':
+            self.zdb.stop()
+        else:
+            self.zdb.namespace_delete(self.disk.data['name'])
+        time.sleep(30)
 
-        #. Create disk [D1] with default data.
-        #. Create vm [VM1] with default values, should succeed.
-        #. Add zerotier network to VM1, should succeed.
-        #. Attach disk [D1] to vm [VM1].
-        #. deploy vm [VM1].
-        #. Check that disk [D1] is attached to vm [VM1], should succeed.
-        #. Remove the disk[D1] from the vm ,should succeed.
-
-        """
-        self.log("Create zdb with default values.")
-        zdb_name = self.random_string()
-        zdb = self.zdb(node=self.node_sal)
-        zdb.data = self.set_zdb_default_data(name=zdb_name)
-        zdb.install()
-        self.zdbs.append(zdb)
-
-        self.log("Create vm [VM1] with default values, should succeed.")
-        vm = self.vm(node=self.node_sal)
-        vm.data = self.set_vm_default_values(os_type="ubuntu")
-        vm.generate_vm_sal()
-
-        for _ in range(10):
-            self.log("Create disk [D1] with default data")
-            disk = Vdisk(node=self.node_sal, zdb=zdb.zerodb_sal)
-            disk.data = self.set_vdisk_default_data()
-            disk.install()
-
-            dir_name = self.random_string()
-            label = self.random_string()
-            self.log("Attach disk [D1] to vm [VM1].")
-            vm.add_disk(disk.disk, mountpoint='/mnt/{}'.format(dir_name), filesystem='ext4', label=label)
-        
-
-        self.log(" Add zerotier network to VM1, should succeed.")
-        vm.add_zerotier_nics(network=self.zt_network)
-        
-        
-        self.log("deploy vm [VM1].")
-        vm.install()
-
-        ztIdentity = vm.data["ztIdentity"]
-        vm_zt_ip = self.get_zerotier_ip(ztIdentity)
-        result = self.ssh_vm_execute_command(vm_ip=vm_zt_ip, cmd='pwd')
-        self.assertEqual(result, '/root')
-
-        import ipdb; ipdb.set_trace()
-        self.vms.append(vm.info()['uuid'])
-        self.zdbs.append(zdb)
+        cmd = 'echo {} > {}/{}'.format(self.random_string(), self.mount_path, self.random_string())
+        result = self.execute_command(ip=vm_zt_ip, cmd=cmd)
+        self.assertTrue(result.returncode)
