d15d6a32aa5aa4da1b69a21c3db4c2307fd861c8
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-10-12 15:44:08 +0200

    zdbclient: move patch outside of the class

--- JumpscaleLib/clients/zdb/ZDBClientBase.py
+++ JumpscaleLib/clients/zdb/ZDBClientBase.py
@@ -4,10 +4,10 @@ from .ZDBMeta import ZDBMeta
 
 JSBASE = j.application.JSBaseClass
 
+
 class ZDBClientBase(JSBASE):
 
-
-    def __init__(self, addr="localhost",port=9900,mode="seq",secret="",nsname="default"):
+    def __init__(self, addr="localhost", port=9900, mode="seq", secret="", nsname="default"):
         """ is connection to ZDB
 
         port {[int} -- (default: 9900)
@@ -19,41 +19,28 @@ class ZDBClientBase(JSBASE):
         self.port = int(port)
         self.mode = mode
         self.secret = secret
-
-        self.redis = j.clients.redis.get(ipaddr=addr,port=port,fromcache=False)
-
         self.type = "ZDB"
 
-        self.redis = self._patch_redis_client(self.redis)
+        self.redis = _patch_redis_client(j.clients.redis.get(ipaddr=addr, port=port, fromcache=False))
 
         self.nsname = nsname.lower().strip()
 
         self.logger_enable()
 
-        if secret != "" and nsname=="default":
-            self.logger.debug("AUTH %s"%(self.nsname))
+        if secret != "" and nsname == "default":
+            self.logger.debug("AUTH %s" % (self.nsname))
             # self.logger.debug("AUTH %s (%s)"%(self.nsname,self.secret))
             self.redis.execute_command("AUTH", self.secret)
 
-
         if self.nsname != "default":
             if self.secret is "":
-                self.logger.debug("select namespace:%s with NO secret"%(self.nsname))
+                self.logger.debug("select namespace:%s with NO secret" % (self.nsname))
                 self.redis.execute_command("SELECT", self.nsname)
             else:
-                self.logger.debug("select namespace:%s with a secret"%(self.nsname))
+                self.logger.debug("select namespace:%s with a secret" % (self.nsname))
                 self.redis.execute_command("SELECT", self.nsname, self.secret)
 
             self.meta = ZDBMeta(self)
-
-    def _patch_redis_client(self, redis):
-        # don't auto parse response for set, it's not 100% redis compatible
-        # 0-db does return a key after in set
-        if 'SET' in redis.response_callbacks:
-            del redis.response_callbacks['SET']
-            del redis.response_callbacks['DEL']
-        return redis
-
 
     def ping(self):
         """
@@ -61,3 +48,12 @@ class ZDBClientBase(JSBASE):
         :return:
         """
         return self.redis.ping()
+
+
+def _patch_redis_client(redis):
+    # don't auto parse response for set, it's not 100% redis compatible
+    # 0-db does return a key after in set
+    for cmd in ['SET', 'DEL']:
+        if cmd in redis.response_callbacks:
+            del redis.response_callbacks[cmd]
+    return redis
