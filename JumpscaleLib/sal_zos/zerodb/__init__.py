0c46710b69cadc2a424e56eb34dcb291b1442701
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-10-12 12:04:39 +0200

    review disk reservation and storage pool creation

--- JumpscaleLib/sal_zos/zerodb/__init__.py
+++ JumpscaleLib/sal_zos/zerodb/__init__.py
@@ -62,86 +62,76 @@ class Zerodbs(DynamicCollection):
         """
         return Zerodb(self.node, name, path, mode, sync, admin)
 
-    def partition_and_mount_disks(self):
+    def prepare(self):
+        """
+        prepare all the disk of the node to be usable by 0-db
+
+        creates a storage pool on each disk, then a filesystem on each storage pool
+
+        :return: list of filesystem path created
+        :rtype: [str]
+        """
+
         mounts = []
 
-        for disk in self.node.disks.list():
-            if disk.type not in [StorageType.HDD, StorageType.SSD, StorageType.NVME, StorageType.ARCHIVE]:
-                logger.info("skipping unsupported disk type %s" % disk.type)
-                continue
+        # list of all disk devices name used by the storage pool existing on the nodes
+        storagepools = self.node.storagepools.list()
+        disks_used = [sp.device[:-1] for sp in storagepools]
+        # list of all disk device name on the nodes
+        disks = self.node.disks.list()
+        all_disks = [d.devicename for d in filter(_zdb_friendly, disks)]
 
-            # this check is there to be able to test with a qemu setup. Not needed if you start qemu with --nodefaults
-            if disk.model in ['QEMU HARDDISK   ', 'QEMU DVD-ROM    ']:
-                continue
-
-            # temporary fix to ommit overwriting the usb boot disk
-            if disk.transport == 'usb':
-                continue
-
-            logger.info("processing disk %s" % disk.devicename)
-
+        # create a storage pool on all the disk which doesn't any storage pool yet
+        for device in set(all_disks) - set(disks_used):
             name = j.data.idgenerator.generateGUID()
-            if not disk.partitions:
-                logger.info("create storage pool on %s" % disk.devicename)
-                sp = self.node.storagepools.create(name, device=disk.devicename, metadata_profile='single', data_profile='single', overwrite=True)
-            else:
-                if len(disk.partitions) > 1:
-                    raise RuntimeError('Found more than 1 partition for disk %s' % disk.name)
+            logger.info("create storage pool %s on %s", name, device)
+            sp = self.node.storagepools.create(name, device=device, metadata_profile='single', data_profile='single', overwrite=True)
+            storagepools.append(sp)
 
-                partition = disk.partitions[0]
-                sps = self.node.storagepools.list(partition.fs_uuid)
-                if len(sps) > 1:
-                    raise RuntimeError('Found more than 1 storagepool for fs_uuid %s' % partition.fs_uuid)
-                elif not sps:
-                    logger.info("create storage pool on %s" % disk.devicename)
-                    sp = self.node.storagepools.create(name, device=disk.devicename, metadata_profile='single', data_profile='single', overwrite=True)
-                else:
-                    sp = sps[0]
-
+        # at this point we have a storage pool on each eligible disk
+        for sp in storagepools:
             if not sp.mountpoint:
+                logger.info("mount storagepool %s", sp.name)
                 sp.mount()
-            if not sp.exists(sp.name):
-                logger.info("create filesystem on %s" % disk.devicename)
-                sp.create(sp.name)
-
-            mounts.append({'disk': disk.name, 'mountpoint': sp.mountpoint})
+            if not sp.exists("zdb"):
+                logger.info("create filesystem on storage pool %s", sp.name)
+                fs = sp.create("zdb")
+            else:
+                fs = sp.get("zdb")
+            mounts.append(fs.path)
 
         return mounts
 
     def create_and_mount_subvolume(self, zdb_name, size, disktypes):
+        GiB = 1024 ** 3
         # filter storagepools that have the correct disk type and whose (total size - reserved subvolume quota) >= size
-        storagepools = list(filter(lambda sp: self.node.disks.get_device(sp.device).disk.type.value in disktypes and (sp.size - sp.total_quota() / (1024 ** 3)) >= size,
-                                   self.node.storagepools.list()))
+
+        def usable_storagepool(sp):
+            if sp.type.value not in disktypes:
+                return False
+            if (sp.size - sp.total_quota() / GiB) <= size:
+                return False
+            return True
+
+        storagepools = list(filter(usable_storagepool, self.node.storagepools.list()))
+        # sort less used pool firt
         storagepools.sort(key=lambda sp: sp.size - sp.total_quota(), reverse=True)
         if not storagepools:
             return ''
 
         storagepool = storagepools[0]
-        # QUESTION: why this? *3 it's not, it's **3 which is "to the power"
-        # so the size is specified in gigabytes (1024x1024x1024)
-        fs = storagepool.create('zdb_{}'.format(zdb_name), size * (1024 ** 3))
-        mount_point = '/mnt/zdbs/{}'.format(zdb_name)
-        self.node.client.filesystem.mkdir(mount_point)
-        subvol = 'subvol={}'.format(fs.subvolume)
-        self.node.client.disk.mount(storagepool.devicename, mount_point, [subvol])
+        fs = storagepool.create('zdb_{}'.format(zdb_name), size * GiB)
+        return fs.path
 
-        return mount_point
 
-    def mount_subvolume(self, zdb_name, mount_point):
-        if self.node.client.filesystem.exists(mount_point):
-            node_mountpoints = self.node.client.disk.mounts()
-            for device in node_mountpoints:
-                for mp in node_mountpoints[device]:
-                    if mp['mountpoint'] == mount_point:
-                        return
-
-        old_zdb = zdb_name.split('_')[-1]  # this is for backward compatability
-        for storagepool in self.node.storagepools.list():
-            for fs in storagepool.list():
-                if fs.name == 'zdb_{}'.format(zdb_name) or fs.name == 'zdb_{}'.format(old_zdb):
-                    self.node.client.filesystem.mkdir(mount_point)
-                    subvol = 'subvol={}'.format(fs.subvolume)
-                    self.node.client.disk.mount(storagepool.devicename, mount_point, [subvol])
-                    break
-        else:
-            raise RuntimeError('Failed to find filesystem for zerodb {}'.format(zdb_name))
+def _zdb_friendly(disk):
+    """
+    filter function to remove disk not suitable for zerodb usage
+    """
+    if disk.type not in [StorageType.HDD, StorageType.SSD, StorageType.NVME, StorageType.ARCHIVE]:
+        logger.info("skipping unsupported disk type %s" % disk.type)
+        return False
+    # this check is there to be able to test with a qemu setup. Not needed if you start qemu with --nodefaults
+    if disk.model in ['QEMU HARDDISK   ', 'QEMU DVD-ROM    '] or disk.transport == 'usb':
+        return False
+    return True
