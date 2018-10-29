6788af8b0b5ee973de17c2aa6570581bce3e382d
Maxime Daniel <root@maxux.net>
2018-10-29 11:19:36 +0100

    Better support of email during farm registration

--- JumpscaleLib/servers/grid_capacity/server/flask_itsyouonline.py
+++ JumpscaleLib/servers/grid_capacity/server/flask_itsyouonline.py
@@ -78,7 +78,6 @@ def requires_auth(org_from_request=False, email=False):
         """
         Wraps route handler to be only accessible after authentication via Itsyou.Online
         """
-
         @wraps(handler)
         def _wrapper(*args, **kwargs):
             if not session.get("_iyo_authenticated"):
@@ -105,9 +104,11 @@ def requires_auth(org_from_request=False, email=False):
                             username = jwt_info["username"]
                             session["iyo_user_info"] = _get_info(username, jwt=jwt_string)
                             session["_iyo_authenticated"] = time.time()
-                            session['iyo_jwt'] = jwt_string
+                            session["iyo_jwt"] = jwt_string
                             return handler(*args, **kwargs)
+
                     return "Could not authorize this request!", 403
+
                 state = str(uuid.uuid4())
                 session["_iyo_state"] = state
                 session['_iyo_organization'] = organization
@@ -158,9 +159,11 @@ def _callback():
     response.raise_for_status()
     response = response.json()
     scope_parts = response["scope"].split(",")
+
     if not "user:memberof:{}".format(authorg) in scope_parts:
         flash("User is not authorized", "danger")
         return redirect("/")
+
     access_token = response["access_token"]
     username = response["info"]["username"]
     # Get user info
