b9898d4756009e019fc7cbeaf4e8eabd958f7803
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-11-05 10:44:12 +0100

    coredns sal: refactor
    - simplify the code
    - support multiple ip for same domain

--- JumpscaleLib/sal/coredns/CoreDnsClient.py
+++ JumpscaleLib/sal/coredns/CoreDnsClient.py
@@ -1,22 +1,28 @@
-from jumpscale import j
 import json
-from .encoding import (encode_record,unregister_record,load)
-from .RoundRobin import zone_exists_list,zone_exists_etcd
-from .ResourceRecord import ResourceRecord ,RecordType
+from ipaddress import (AddressValueError, IPv4Address, IPv6Address,
+                       NetmaskValueError)
+from urllib.parse import urlparse
+
+from jumpscale import j
+
+from .ResourceRecord import RecordType, ResourceRecord
+
+
 JSConfigBase = j.tools.configmanager.base_class_config
 
 TEMPLATE = """
 etcd_instance = "main"
 """
+
+
 class CoreDnsClient(JSConfigBase):
 
     def __init__(self, instance, data={}, parent=None, interactive=False):
-        JSConfigBase.__init__(self, instance=instance,
-                              data=data, parent=parent, template=TEMPLATE, interactive=interactive)
+        JSConfigBase.__init__(self, instance=instance, data=data, parent=parent,
+                              template=TEMPLATE, interactive=interactive)
         self._etcd_client = None
-        print ("CoreDNS", instance)
+        print("CoreDNS", instance)
         self._zones = []
-
 
     @property
     def etcd_client(self):
@@ -26,13 +32,13 @@ class CoreDnsClient(JSConfigBase):
 
     @property
     def zones(self):
-        """get all zones 
+        """get all zones
         """
         if not self._zones:
-            self._zones = load(self.etcd_client)
+            self._zones = _load(self.etcd_client)
         return self._zones
 
-    def zone_create(self, domain, rrdata, record_type=RecordType.A,  ttl=300, priority=None, port=None ):
+    def zone_create(self, domain, rrdata, record_type=RecordType.A, ttl=300, priority=None, port=None):
         """ create new zone .
 
             domain    : Record Name.  Corresponds to first field in
@@ -50,12 +56,6 @@ class CoreDnsClient(JSConfigBase):
             CNAME record example: rrdata = 'cn1.skydns.local skydns.local.'
         """
         zone = ResourceRecord(domain, rrdata, record_type, ttl, priority, port)
-        etcd_x = zone_exists_etcd(zone,self.etcd_client)
-        list_x = zone_exists_list(zone,self._zones)
-        if etcd_x >list_x:
-            zone.domain = 'x{}.{}'.format(etcd_x,zone.domain)
-        else:
-            zone.domain = 'x{}.{}'.format(list_x,zone.domain)
         self._zones.append(zone)
         return zone
 
@@ -64,16 +64,93 @@ class CoreDnsClient(JSConfigBase):
         add coredns records in etcd
         :param zones: list of `ResourceRecord` objects that needs to be added
         """
+        per_domain = {}
         for zone in self.zones:
-            key, value = encode_record(zone)
-            self.etcd_client.put(key, value)
-    
+            if zone.domain not in per_domain:
+                per_domain[zone.domain] = [zone]
+            else:
+                per_domain[zone.domain].append(zone)
+
+        for zones in per_domain.values():
+            if len(zones) > 1:
+                for i, zone in enumerate(zones):
+                    zone.domain = 'x%d.%s' % (i, zone.domain)
+
+        for _, zones in per_domain.values():
+            self.etcd_client.put(zone.key(), zone.rdata)
+
     def remove(self, zones):
         """
         remove coredns records from etcd
         :param zones: list of `ResourceRecord` objects that needs to be removed
         """
         for zone in zones:
-            key = unregister_record(zone)
-            self.etcd_client.api.delete_prefix(key)
+            self.etcd_client.api.delete_prefix(zone.key())
 
+
+def _load(client):
+    """
+    get all zones from etcd
+
+    Arguments:
+        client : ETCD client
+    """
+    zones = []
+    for value, metadata in client.api.get_prefix('/hosts'):
+        ss = metadata.key.decode().split('/')
+
+        domain = '.'.join(reversed(ss[2:]))
+        value = json.loads(value.decode())
+
+        type_of_record, rrdata = _get_type_and_rdata(value)
+
+        if type_of_record == "SRV":
+            zone = ResourceRecord(domain, rrdata, type_of_record, value["ttl"], value["priority"], value["port"])
+        else:
+            zone = ResourceRecord(domain, rrdata, type_of_record, value["ttl"])
+
+        # zone.domain = _sanitize_domain(zone.domain)
+        zones.append(zone)
+    return zones
+
+
+def _get_type_and_rdata(record):
+    """get type and data of record
+
+    Arguments:
+        record: dict value of zone
+
+    Returns: type and data of type
+    """
+    if "text" in record:
+        return 'TXT', record['text']
+    elif "port" in record:
+        return 'SRV', record['host']
+    elif "cname" in record:
+        return 'CNAME', record['cname']
+    elif "host" in record:
+        rrdata = record['host']
+        try:
+            IPv6Address(rrdata)
+            rtype = 'AAAA'
+            return rtype, rrdata
+        except (AddressValueError, NetmaskValueError):
+            try:
+                IPv4Address(rrdata)
+                rtype = 'A'
+                return rtype, rrdata
+            except (AddressValueError, NetmaskValueError):
+                pass
+    raise KeyError("cannot identify record type %s" % repr(record))
+
+
+def _sanitize_domain(domain):
+    if domain[0] != 'x':
+        return domain
+
+    prefix, rest = domain.split('.', 1)
+    try:
+        int(prefix[1:])
+        return rest
+    except TypeError:
+        return domain
--- JumpscaleLib/sal/coredns/ResourceRecord.py
+++ JumpscaleLib/sal/coredns/ResourceRecord.py
@@ -1,17 +1,19 @@
 import json
 from enum import Enum
 
+
 class RecordType(Enum):
     A = "A"
     AAAA = "AAAA"
     SRV = "SRV"
     TXT = "TXT"
 
+
 class ResourceRecord:
 
     def __init__(self, domain, rrdata, record_type=RecordType.A, ttl=300,
-                             priority=None, port=None # SRV
-                ):
+                 priority=None, port=None  # SRV
+                 ):
         """ DNS Resource Record.
 
             domain    : Record Name.  Corresponds to first field in
@@ -42,23 +44,33 @@ class ResourceRecord:
         self.weight = 100
         self._rrdata = rrdata
 
-
     @property
     def rrdata(self):
-        """ reconstructs CoreDNS/etcd-style JSON data format
+        """
+        reconstructs CoreDNS/etcd-style JSON data format
         """
         res = {'ttl': self.ttl}
-        rdatafield = 'host' # covers SRV, A, AAAA and CNAME
+        rdatafield = 'host'  # covers SRV, A, AAAA and CNAME
         if self.type == RecordType.TXT:
             rdatafield = 'text'
         elif self.type == RecordType.SRV:
             res['priority'] = self.priority
             res['port'] = self.port
         elif self.type == 'CNAME':
-            res['cname'] = self._rrdata 
-            res['host'] = self.cname  
+            res['cname'] = self._rrdata
+            res['host'] = self.cname
         res[rdatafield] = self._rrdata
         return json.dumps(res)
+
+    def key(self):
+        """
+        return the key used to store this entry in etcd
+        """
+        domain_parts = self.domain.split('.')
+        # The key for coredns should start with path(/hosts) and the domain reversed
+        # i.e. test.com => /hosts/com/test
+        key = "/hosts/{}".format("/".join(domain_parts[::-1]))
+        return key
 
     def __str__(self):
         if self.type == 'TXT':
@@ -72,4 +84,4 @@ class ResourceRecord:
             (self.domain, self.ttl, extra, rrdata)
 
     def __repr__(self):
-            return "'%s'" % str(self)
+        return "'%s'" % str(self)
