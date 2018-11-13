9dc3e7f8bd8b6d2d061671a351af7d17ed0a95c6
Maxime Daniel <root@maxux.net>
2018-11-13 03:59:53 +0100

    Improve container export to support upload via token

--- JumpscaleLib/clients/zero_os_protocol/FlistManager.py
+++ JumpscaleLib/clients/zero_os_protocol/FlistManager.py
@@ -7,19 +7,21 @@ class FlistManager:
         self._client = client
         self._id = container_id
 
-    def create(self, src, dst, storage):
+    def create(self, src, dst, storage, token=""):
         """
         Create an flist from src
-        
+
         :param src: Absolute path of the directory with the files that will be uploaded to storage
         :param dst: Filst name (Ex: /tmp/myflist.flist)
         :param storage: Address of zdb were files will be uploaded
+        :param token: An optional iyo jwt token to upload data into the backend and flist on the hub
         """
         args = {
             'container': self._id,
             'flist': dst,
             'src': src,
             'storage': storage,
+            'token': token,
         }
         flist_loction = self._client.json("corex.flist.create", args)
         return flist_loction.split('/mnt/containers/%d/' % self._id)[1]
