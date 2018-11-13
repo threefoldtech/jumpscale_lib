f59a691838127b5b9d0ba88ac03e2fa4c97a20cd
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-11-13 12:58:51 +0100

    sal_zos: move tfchain factory to proper file

--- JumpscaleLib/sal_zos/tfchain/TfChainFactory.py
+++ JumpscaleLib/sal_zos/tfchain/TfChainFactory.py
@@ -1,4 +1,4 @@
-from .TfChain import TfChain
+from .TfChain import TfChainDaemon, TfChainExplorer, TfChainClient
 from jumpscale import j
 
 JSBASE = j.application.jsbase_get_class()
@@ -9,11 +9,11 @@ class TfChainFactory(JSBASE):
         self.__jslocation__ = "j.sal_zos.tfchain"
         JSBASE.__init__(self)
 
-    @staticmethod
-    def get():
-        """
-        Get sal for tfchain
-        Returns:
-            the sal layer 
-        """
-        return TfChain()
+    def daemon(self, name, container, data_dir='/mnt/data', rpc_addr='0.0.0.0:23112', api_addr='localhost:23110', network='standard'):
+        return TfChainDaemon(name=name, container=container, data_dir=data_dir, rpc_addr=rpc_addr, api_addr=api_addr, network=network)
+
+    def explorer(self, name, container, domain, data_dir='/mnt/data', rpc_addr='0.0.0.0:23112', api_addr='localhost:23110', network='standard'):
+        return TfChainExplorer(name=name, container=container, data_dir=data_dir, rpc_addr=rpc_addr, api_addr=api_addr, domain=domain, network=network)
+
+    def client(self, name, container, wallet_passphrase, api_addr='localhost:23110'):
+        return TfChainClient(name=name, container=container, addr=api_addr, wallet_passphrase=wallet_passphrase)
