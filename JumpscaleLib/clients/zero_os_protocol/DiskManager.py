d50d568d6be3dc9be6b1002f09e2033416f0d815
Jan De Landtsheer <jan@delandtsheer.eu>
2018-12-17 11:57:33 +0100

    added call for raw disk.isstandby

--- JumpscaleLib/clients/zero_os_protocol/DiskManager.py
+++ JumpscaleLib/clients/zero_os_protocol/DiskManager.py
@@ -50,6 +50,10 @@ class DiskManager:
         'disk': str,
     })
 
+    _isstandby_chk = typchk.Checker({
+        'disk': str,
+    })
+
     def __init__(self, client):
         self._client = client
 
@@ -305,6 +309,21 @@ class DiskManager:
         result = response.get()
         if result.state != 'SUCCESS':
             raise RuntimeError("Failed to spindown disk {} to {}.".format(disk, spindown))
+
+    def isstandby(self, disk):
+        """
+        Verify if a disk is powered down (i.e. it's still available, but went
+        in standby mode, and stopped spinning after spindown (see above) interval)
+        NOTE: default of spindown is 1, so after 5 seconds, and not accessed the disk
+        will stop spinning
+        :param disk str: Full path to a disk like /dev/sda
+        """
+        args = {
+            "disk": disk,
+        }
+        self._isstandby_chk.check(args)
+
+        return self._client.json("disk.isstandby",args)
 
     def seektime(self, disk):
         """
