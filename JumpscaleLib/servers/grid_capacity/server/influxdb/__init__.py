bf26886b270e47a1de442d330ec7c72e8f053871
Maxime Daniel <ls@maxux.net>
2019-02-20 16:28:07 +0000

    Improve influxdb initializer code

--- JumpscaleLib/servers/grid_capacity/server/influxdb/__init__.py
+++ JumpscaleLib/servers/grid_capacity/server/influxdb/__init__.py
@@ -3,33 +3,28 @@ from jumpscale import j
 _client = None
 influxlog = j.logger.get("grid-influx")
 
-def init(settings):
-    if not settings.INFLUX_HOST:
-        return False
-
-    if not settings.INFLUX_DB:
-        return False
-
+def init(host, port, db):
     global _client
-    _client = j.clients.influxdb.get(settings.INFLUX_DB, {
-        'host': settings.INFLUX_HOST,
-        'port': settings.INFLUX_PORT,
+    _client = j.clients.influxdb.get(db, {
+        'host': host,
+        'port': port,
         'username': "root",
         'password': "",
-        'database': settings.INFLUX_DB
+        'database': db
     })
 
-    for db in _client.get_list_database():
-        if db['name'] == settings.INFLUX_DB:
-            influxlog.notice("database <%s> already exists, using it" % settings.INFLUX_DB)
+    for exists in _client.get_list_database():
+        if exists['name'] == db:
+            influxlog.info("database <%s> already exists, using it" % db)
             return True
 
-    influxlog.notice("creating database <%s>" % settings.INFLUX_DB)
-    _client.create_database(settings.INFLUX_DB)
+    influxlog.info("creating database <%s>" % db)
+    _client.create_database(db)
 
 
 def write(data):
     global _client
     if not _client:
         return
+
     _client.write_points(data)
