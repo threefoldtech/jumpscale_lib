9ac1e7c612066878aeef9a876d1f630eaa2edcfa
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-05-28 09:31:26 +0200

    zos capacity: allow to send hardware dump to the directory

--- JumpscaleLib/sal_zos/capacity/Capacity.py
+++ JumpscaleLib/sal_zos/capacity/Capacity.py
@@ -1,4 +1,5 @@
 import io
+import re
 
 import requests
 
@@ -6,11 +7,90 @@ from jumpscale import j
 
 logger = j.logger.get(__name__)
 
+_DISK_DUMP_EXPRESSION = re.compile(r"^([^\s]+)\s+-d\s+([^\s]+)\s+#")
+_AGGREGATOR_NAME = "minting-py smartctl aggregator v0.1.0"
+_HEADER_EXPRESSION = re.compile(r"([^\[]+)\[([^\[]+)\]")
+_INFORMATION_EXPRESSION = re.compile(r"([^:]+):\s+(.+)")
+
+
+def parse_smartctl_stdout(stdout):
+    output = {}
+    lines = stdout.splitlines()
+    if len(lines) == 0:
+        return output
+
+    header = _HEADER_EXPRESSION.search(lines[0])
+    if len(lines) == 1:
+        return output
+
+    output["tool"] = header.group(1).strip()
+    output["environment"] = header.group(2).strip()
+    information = {}
+    for line in lines[1:]:
+        pair = _INFORMATION_EXPRESSION.search(line)
+        if not pair:
+            continue
+        information[pair.group(1).strip()] = pair.group(2).strip()
+    output["information"] = information
+    return output
+
 
 class Capacity:
 
     def __init__(self, node):
         self._node = node
+
+    def hardware_dump(self):
+        output = self._node.client.info.dmi()
+        if isinstance(output, dict) and "tooling" in output:
+            return output
+        # hardcode tooling section if it is not yet available in this node
+        return {
+            "tooling": {
+                "aggregator": "dmidecode 3.1",
+                "decoder": "0-core Go dmi decoder v0.0.9",
+            },
+            "sections": output,
+        }
+
+    def disks_dump(self):
+        # fetch all the disks
+        buff = io.StringIO()
+
+        def callback(level, msg, flag):
+            buff.write(msg)
+
+        # we stream the result in case there are a lot of disk
+        # and we cannot get the full output using job.get()
+        job = self._node.client.system("smartctl --scan", stream=True)
+        job.stream(callback)
+
+        disks = {}
+        for line in buff.getvalue().splitlines():
+            match = _DISK_DUMP_EXPRESSION.search(line)
+            if not match:
+                continue
+            disks[match.group(1)] = match.group(2)
+        dump = []
+        for disk_name, disk_type in disks.items():
+            result = self._node.client.system("smartctl -i %s -d %s" % (disk_name, disk_type)).get()
+            dump.append(parse_smartctl_stdout(result.stdout))
+        if len(dump) == 0:
+            return {}
+        # aggregate disks, so we only have to print tool/environment info once
+        info = {
+            "tool": dump[0]["tool"],
+            "environment": dump[0]["environment"],
+            "aggregator": _AGGREGATOR_NAME,
+            "devices": [],
+        }
+        for disk in dump:
+            if disk["tool"] != info["tool"]:
+                raise RuntimeError("unexpected tool: {} != {}".format(disk["tool"], info["tool"]))
+            if disk["environment"] != info["environment"]:
+                raise RuntimeError("unexpected environment: {} != {}".format(disk["environment"], info["environment"]))
+            info["devices"].append(disk["information"])
+        return info
 
     def total_report(self):
         """
@@ -56,7 +136,7 @@ class Capacity:
         # return production directory
         return j.clients.threefold_directory.get(interactive=False)
 
-    def register(self):
+    def register(self, include_hardware_dump=False):
         farmer_id = self._node.kernel_args.get('farmer_id')
         if not farmer_id:
             return False
@@ -80,6 +160,11 @@ class Capacity:
             parameters=parameters,
             uptime=int(self._node.uptime())
         )
+
+        if include_hardware_dump:
+            data['hardware'] = self.hardware_dump()
+            data['disks'] = self.disks_dump()
+
         data['farmer_id'] = farmer_id
 
         if 'private' in self._node.kernel_args:
