37997ca6f94b94ae35c1bda72826fee1dd6aaccb
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-08-05 14:51:41 +0200

    capcity directory stability and performence improvement
    - add pagination to the api: we limite the number of reponse when
    listing nodes
    - update client so it consume all the page from the api automatically so
    we don't change the default behavior
    - add some index in the mongo model to speed up queries

--- JumpscaleLib/servers/grid_capacity/server/handlers/ListCapacityHandler.py
+++ JumpscaleLib/servers/grid_capacity/server/handlers/ListCapacityHandler.py
@@ -8,25 +8,36 @@ from io import StringIO
 def ListCapacityHandler():
 
     nodes = []
-    country = request.values.get('country')
-    mru = request.values.get('mru')
-    cru = request.values.get('cru')
-    hru = request.values.get('hru')
-    sru = request.values.get('sru')
-    farmer = request.values.get('farmer')
+    country = request.values.get("country")
+    mru = request.values.get("mru")
+    cru = request.values.get("cru")
+    hru = request.values.get("hru")
+    sru = request.values.get("sru")
+    farmer = request.values.get("farmer")
+    page = int(request.values.get("page") or 1)
     nodes = NodeRegistration.search(country, mru, cru, hru, sru, farmer, fulldump=True)
 
     output = []
-    with_proofs = request.args.get('proofs')
+    with_proofs = request.args.get("proofs")
+    if not with_proofs:
+        nodes = nodes.exclude("proofs")
 
-    for node in nodes.all():
+    max_per_page = 100
+    total = nodes.count()
+    per_page = min(max_per_page, (max_per_page + (total - (page * max_per_page))))
+    if per_page < 0:
+        per_page = 0
+
+    paginated = nodes.paginate(page=page, per_page=per_page)
+
+    for node in paginated.items:
         if node.farmer.location and node.farmer.location.latitude and node.farmer.location.longitude:
             node.location = node.farmer.location
         d = node.to_mongo().to_dict()
-        d['node_id'] = d.pop('_id')
-        d['farmer_id'] = d.pop('farmer')
-        if not with_proofs and 'proofs' in d:
-            d.pop('proofs')
+        d["node_id"] = d.pop("_id")
+        d["farmer_id"] = d.pop("farmer")
         output.append(d)
 
-    return jsonify(output)
+    resp = jsonify(output)
+    resp.headers.set("page", str(paginated.pages))
+    return resp
