cf0c6f73ae8ca30e78415a439739f31706749e4e
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-08-07 18:10:37 +0200

    directory: allow to choose the number of item return when listing nodes

--- JumpscaleLib/servers/grid_capacity/server/handlers/ListCapacityHandler.py
+++ JumpscaleLib/servers/grid_capacity/server/handlers/ListCapacityHandler.py
@@ -3,6 +3,7 @@
 from ..models import NodeRegistration
 from flask import request, jsonify
 from io import StringIO
+import math
 
 
 def ListCapacityHandler():
@@ -22,15 +23,15 @@ def ListCapacityHandler():
     if not with_proofs:
         nodes = nodes.exclude("proofs")
 
-    max_per_page = 100
-    total = nodes.count()
-    per_page = min(max_per_page, (max_per_page + (total - (page * max_per_page))))
-    if per_page < 0:
-        per_page = 0
+    max_per_page = int(request.args.get('per_page', 100))
+    if not max_per_page or max_per_page > 100:
+        max_per_page = 100
 
-    paginated = nodes.paginate(page=page, per_page=per_page)
+    offset = (page - 1) * max_per_page
+    paginated = nodes.skip(offset).limit(max_per_page)
+    nr_page = int(math.ceil(nodes.count() / float(max_per_page)))
 
-    for node in paginated.items:
+    for node in paginated:
         if node.farmer.location and node.farmer.location.latitude and node.farmer.location.longitude:
             node.location = node.farmer.location
         d = node.to_mongo().to_dict()
@@ -39,5 +40,5 @@ def ListCapacityHandler():
         output.append(d)
 
     resp = jsonify(output)
-    resp.headers.set("page", str(paginated.pages))
+    resp.headers.set("page", str(nr_page))
     return resp
